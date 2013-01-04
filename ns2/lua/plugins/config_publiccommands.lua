b64ce6868f79d32b64bd620e32aa7b6e48aad6d6
Peter Kormendi <kormendi@gmail.com>
2013-01-04 01:11:20 -0500

    added broadcast to all players and throttle

--- ns2/lua/plugins/config_publiccommands.lua
+++ ns2/lua/plugins/config_publiccommands.lua
@@ -6,9 +6,14 @@ local function SetupDefaultConfig(Save)
 		kDAKConfig.PublicCommands = { }
 	end
 	kDAKConfig.PublicCommands.Commands = {}
-	// [feature] = command string
-	kDAKConfig.PublicCommands.Commands["timeleft"] = "timeleft"
-	kDAKConfig.PublicCommands.Commands["nextmap"] = "nextmap"
+	// ["feature"].command = "command string"
+	// ["feature"].throttle = min seconds between execution of command
+	kDAKConfig.PublicCommands.Commands["timeleft"] = { }
+	kDAKConfig.PublicCommands.Commands["timeleft"].command = "timeleft"
+	kDAKConfig.PublicCommands.Commands["timeleft"].throttle = 30
+	kDAKConfig.PublicCommands.Commands["nextmap"] = { }
+	kDAKConfig.PublicCommands.Commands["nextmap"].command = "nextmap"
+	kDAKConfig.PublicCommands.Commands["nextmap"].throttle = 30
 	
 	if Save then
 		SaveDAKConfig()
--- ns2/lua/plugins/plugin_publiccommands.lua
+++ ns2/lua/plugins/plugin_publiccommands.lua
@@ -5,32 +5,42 @@ if kDAKConfig and kDAKConfig.PublicCommands and kDAKConfig.PublicCommands.Comman
 	local TIMELEFTCOMMAND = "timeleft"
 	local NEXTMAPCOMMAND = "nextmap"
 
+	local function GetPlayerList()
+
+		local playerList = EntityListToTable(Shared.GetEntitiesWithClassname("Player"))
+		table.sort(playerList, function(p1, p2) return p1:GetName() < p2:GetName() end)
+		return playerList
+		
+	end
+
 	//////////////
 	// Timeleft //
 	//////////////
 	
+	local timeleftThrottle = 0
 	local function OnCommandTimeleft(client)
-		if client ~= nil then
-		
-			local player = client:GetControllingPlayer()
-			if player ~= nil then
-				chatMessage = string.sub(string.format("%.1f Minutes Remaining.", math.max(0,((kDAKMapCycle.time * 60) - Shared.GetTime())/60)), 1, kMaxChatLength)
-				Server.SendNetworkMessage(player, "Chat", BuildChatMessage(false, "PM - " .. kDAKConfig.DAKLoader.MessageSender, -1, kTeamReadyRoom, kNeutralTeamType, chatMessage), true)
+		if client ~= nil and Shared.GetTime() - timeleftThrottle > kDAKConfig.PublicCommands.Commands[TIMELEFTCOMMAND].throttle then
+			timeleftThrottle = Shared.GetTime()
+			for _, player in pairs(GetPlayerList()) do
+				if player ~= nil then
+					chatMessage = string.sub(string.format("%.1f Minutes Remaining.", math.max(0,((kDAKMapCycle.time * 60) - Shared.GetTime())/60)), 1, kMaxChatLength)
+					Server.SendNetworkMessage(player, "Chat", BuildChatMessage(false, kDAKConfig.DAKLoader.MessageSender, -1, kTeamReadyRoom, kNeutralTeamType, chatMessage), true)
+				end
 			end
-			
 		end
 		
 	end
 
 	// This command is already defined by the mapvote plugin.  If mapvote is in the plugin list, don't hook it again.
 	if kDAKConfig.PublicCommands.Commands[TIMELEFTCOMMAND] and not kDAKConfig.DAKLoader.kPluginsList["mapvote"] then
-		Event.Hook("Console_" .. kDAKConfig.PublicCommands.Commands[TIMELEFTCOMMAND],               OnCommandTimeleft)
+		Event.Hook("Console_" .. kDAKConfig.PublicCommands.Commands[TIMELEFTCOMMAND].command,               OnCommandTimeleft)
 	end
 	
 	/////////////
 	// Nextmap //
 	/////////////
 	
+	local nextmapThrottle = 0
 	local function GetMapName(map)
 		if type(map) == "table" and map.map ~= nil then
 			return map.map
@@ -40,23 +50,23 @@ if kDAKConfig and kDAKConfig.PublicCommands and kDAKConfig.PublicCommands.Comman
 	
 	local function OnCommandNextMap(client)
 
-		if client ~= nil then
-		
-			local player = client:GetControllingPlayer()
-			if player ~= nil then
-				local mapname = GetMapName(MapCycle_GetNextMapInCycle())
-				if mapname ~= nil then
-					chatMessage = string.sub(string.format("Next map: %s", mapname), 1, kMaxChatLength)
-					Server.SendNetworkMessage(player, "Chat", BuildChatMessage(false, "PM - " .. kDAKConfig.DAKLoader.MessageSender, -1, kTeamReadyRoom, kNeutralTeamType, chatMessage), true)
+		if client ~= nil and Shared.GetTime() - nextmapThrottle > kDAKConfig.PublicCommands.Commands[NEXTMAPCOMMAND].throttle then
+			nextmapThrottle = Shared.GetTime()
+			for _, player in pairs(GetPlayerList()) do
+				if player ~= nil then
+					local mapname = GetMapName(MapCycle_GetNextMapInCycle())
+					if mapname ~= nil then
+						chatMessage = string.sub(string.format("Next map: %s", mapname), 1, kMaxChatLength)
+						Server.SendNetworkMessage(player, "Chat", BuildChatMessage(false, kDAKConfig.DAKLoader.MessageSender, -1, kTeamReadyRoom, kNeutralTeamType, chatMessage), true)
+					end
 				end
 			end
-			
 		end
 		
 	end
 
 	if kDAKConfig.PublicCommands.Commands[NEXTMAPCOMMAND] then
-		Event.Hook("Console_" .. NEXTMAPCOMMAND,               OnCommandNextMap)
+		Event.Hook("Console_" .. kDAKConfig.PublicCommands.Commands[NEXTMAPCOMMAND].command,               OnCommandNextMap)
 	end
 	
 	///////////////////
@@ -68,13 +78,13 @@ if kDAKConfig and kDAKConfig.PublicCommands and kDAKConfig.PublicCommands.Comman
 		if client and steamId and steamId ~= 0 then
 			// Timeleft
 			if kDAKConfig.PublicCommands.Commands[TIMELEFTCOMMAND] and not kDAKConfig.DAKLoader.kPluginsList["mapvote"] and
-				message == kDAKConfig.PublicCommands.Commands[TIMELEFTCOMMAND] then
+				message == kDAKConfig.PublicCommands.Commands[TIMELEFTCOMMAND].command then
 				OnCommandTimeleft(client)
 				return true
 			end
 			
 			// Nextmap
-			if kDAKConfig.PublicCommands.Commands[NEXTMAPCOMMAND] and message == kDAKConfig.PublicCommands.Commands[NEXTMAPCOMMAND] then
+			if kDAKConfig.PublicCommands.Commands[NEXTMAPCOMMAND] and message == kDAKConfig.PublicCommands.Commands[NEXTMAPCOMMAND].command then
 				OnCommandNextMap(client)
 				return true
 			end
