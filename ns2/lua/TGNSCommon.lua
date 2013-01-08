63785e5ec08b58a9721f310c94cc4db224a8bdf9
M. Lance Hilliard <lance@digitalcreations.cc>
2013-01-07 23:32:16 -0600

    added some functions to TGNSCommon

--- ns2/lua/TGNSCommon.lua
+++ ns2/lua/TGNSCommon.lua
@@ -8,6 +8,72 @@ function GetPlayerList()
 
 end
 
+function AllPlayers(doThis)
+
+	return function(client)
+	
+		local playerList = GetPlayerList()
+		for p = 1, #playerList do
+		
+			local player = playerList[p]
+			doThis(player, client, p)
+			
+		end
+		
+	end
+	
+end
+
+function GetPlayerMatchingName(name, team)
+
+	assert(type(name) == "string")
+	
+	local nameMatchCount = 0
+	local match = nil
+	
+	local function Matches(player)
+		if nameMatchCount == -1 then
+			return // exact match found, skip others to avoid further partial matches
+		end
+		local playerName =  player:GetName()
+		if player:GetName() == name then // exact match
+			if team == nil or team == -1 or team == player:GetTeamNumber() then
+				match = player
+				nameMatchCount = -1
+			end
+		else
+			local index = string.find(string.lower(playerName), string.lower(name)) // case insensitive partial match
+			if index ~= nil then
+				if team == nil or team == -1 or team == player:GetTeamNumber() then
+					match = player
+					nameMatchCount = nameMatchCount + 1
+				end
+			end
+		end
+		
+	end
+	AllPlayers(Matches)()
+	
+	if nameMatchCount > 1 then
+		match = nil // if partial match is not unique, clear the match
+	end
+	
+	return match
+
+end
+
+function GetPlayerMatching(id, team)
+
+	local idNum = tonumber(id)
+	if idNum then
+		return GetPlayerMatchingGameId(idNum, team) or GetPlayerMatchingSteamId(idNum, team)
+	elseif type(id) == "string" then
+		return GetPlayerMatchingName(id, team)
+	end
+
+end
+
+
 if kDAKConfig and kDAKConfig.DAKLoader then
 	function PMAllPlayersWithAccess(srcClient, message, command, showCommand)
 		if srcClient then
