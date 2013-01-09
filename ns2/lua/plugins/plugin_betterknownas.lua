1718789bfba43a790401178708b472f954049fb8
M. Lance Hilliard <lance@digitalcreations.cc>
2013-01-08 23:29:00 -0600

    betterknownas: added AKAs

--- ns2/lua/plugins/plugin_betterknownas.lua
+++ ns2/lua/plugins/plugin_betterknownas.lua
@@ -39,23 +39,21 @@ if kDAKConfig and kDAKConfig.BetterKnownAs then
 		return result
 	end
 
-	local function GetMostRecentBka(bkaData)
-		local result = nil
-			if bkaData ~= nil and bkaData.names ~= nil and #bkaData.names > 0 then
-				result = bkaData.names[0]
-			end
-		return result
-	end
-	
 	local function ShowCurrentBka(client, targetSteamId)
-		ServerAdminPrint(client, "[BKA] Current BKA:")
 		local steamId = client:GetUserId()
 		local bkaData = LoadBkaData(targetSteamId)
-		if bkaData == nil or bkaData.names == nil or #bkaData.names == 0 then
+		ServerAdminPrint(client, "[BKA] Current BKA:")
+		if bkaData == nil or bkaData.BKA == nil or string.len(bkaData.BKA) == 0 then
 			ServerAdminPrint(client, "[BKA]     (none)")
 		else
-			for i = 1, #bkaData.names, 1 do
-				local name = bkaData.names[i]
+			ServerAdminPrint(client, "[BKA]     " .. bkaData.BKA)
+		end
+		ServerAdminPrint(client, "[BKA] Current AKAs:")
+		if bkaData == nil or bkaData.AKAs == nil or #bkaData.AKAs == 0 then
+			ServerAdminPrint(client, "[BKA]     (none)")
+		else
+			for i = 1, #bkaData.AKAs, 1 do
+				local name = bkaData.AKAs[i]
 				ServerAdminPrint(client, "[BKA]     " .. name)
 			end
 		end
@@ -64,17 +62,64 @@ if kDAKConfig and kDAKConfig.BetterKnownAs then
 	local function ShowUsage(client, targetSteamId) 
 		ServerAdminPrint(client, "[BKA]")
 		ServerAdminPrint(client, "[BKA] Usage:")
+		ServerAdminPrint(client, "[BKA]     sv_aka <target> <aka>")
 		ServerAdminPrint(client, "[BKA]     sv_bka <target> <bka>")
 		ServerAdminPrint(client, "[BKA] Notes:")
-		ServerAdminPrint(client, "[BKA] * Keep the BKA short.")
-		ServerAdminPrint(client, "[BKA] * Most recent (highest) BKA is enforced.")
-		ServerAdminPrint(client, "[BKA] * <bka> of 'clear' removes all BKA names")
+		ServerAdminPrint(client, "[BKA] * Keep the BKA/AKA short.")
+		ServerAdminPrint(client, "[BKA] * BKA is enforced.")
+		ServerAdminPrint(client, "[BKA] * <bka> of 'clear' removes enforced BKA name")
+		ServerAdminPrint(client, "[BKA] * <aka> of 'clear' removes all AKA names")
 		if targetSteamId ~= nil then
 			ShowCurrentBka(client, targetSteamId)
 		end
 		ServerAdminPrint(client, "[BKA]")
 	end
 	
+	local function svAka(client, playerName, ...)
+		local targetPlayer = GetPlayerMatching(playerName, nil)
+		if targetPlayer ~= nil then
+			local targetClient = Server.GetOwner(targetPlayer)
+			if targetClient ~= nil then
+				local targetSteamId = targetClient:GetUserId()
+				local newBkaName = GetBkaName(...)
+				if newBkaName ~= "" then
+					local existingBkaData = LoadBkaData(targetSteamId)
+					local newBkaData = { steamId = targetSteamId, AKAs = {}, BKA = "" }
+					if newBkaName ~= "clear" then
+						table.insert(newBkaData.AKAs, newBkaName)
+						if existingBkaData ~= nil then
+							if existingBkaData.AKAs ~= nil and #existingBkaData.AKAs > 0 then
+								for i = 1, #existingBkaData.AKAs, 1 do
+									local existingName = existingBkaData.AKAs[i]
+									if existingName ~= newBkaName then
+										table.insert(newBkaData.AKAs, existingName)
+									end
+								end
+							end
+						end
+					end
+					if (existingBkaData ~= nil) then
+						newBkaData.BKA = existingBkaData.BKA
+					end
+					SaveBka(newBkaData)
+					ShowCurrentBka(client, targetSteamId)
+				else
+					ShowUsage(client, targetSteamId)
+				end
+			else
+				ServerAdminPrint(client, string.format("'%s' uniquely matches a player, but no client found.", playerName))
+			end
+		else
+			if playerName == nil then
+				ShowUsage(client, nil)
+			else
+				ServerAdminPrint(client, string.format("'%s' does not uniquely match a player.", playerName))
+			end
+			
+		end
+	end
+	DAKCreateServerAdminCommand("Console_sv_aka", svAka, "<target> <aka> Adds an AKA name to the target.")
+
 	local function svBka(client, playerName, ...)
 		local targetPlayer = GetPlayerMatching(playerName, nil)
 		if targetPlayer ~= nil then
@@ -83,20 +128,18 @@ if kDAKConfig and kDAKConfig.BetterKnownAs then
 				local targetSteamId = targetClient:GetUserId()
 				local newBkaName = GetBkaName(...)
 				if newBkaName ~= "" then
-					local newBkaData = { steamId = targetSteamId, names = {} }
-					if newBkaName ~= "clear" then
-						table.insert(newBkaData.names, newBkaName)
-						local existingBkaData = LoadBkaData(targetSteamId)
-						if existingBkaData ~= nil then
-							if existingBkaData.names ~= nil then
-								for i = 1, #existingBkaData.names, 1 do
-									local existingName = existingBkaData.names[i]
-									if existingName ~= newBkaName then
-										table.insert(newBkaData.names, existingName)
-									end
-								end
+					local existingBkaData = LoadBkaData(targetSteamId)
+					local newBkaData = { steamId = targetSteamId, AKAs = {}, BKA = "" }
+					if existingBkaData ~= nil then
+						if existingBkaData.AKAs ~= nil then
+							for i = 1, #existingBkaData.AKAs, 1 do
+								local existingName = existingBkaData.AKAs[i]
+								table.insert(newBkaData.AKAs, existingName)
 							end
 						end
+					end
+					if newBkaName ~= "clear" then
+						newBkaData.BKA = newBkaName
 					end
 					SaveBka(newBkaData)
 					ShowCurrentBka(client, targetSteamId)
@@ -116,15 +159,15 @@ if kDAKConfig and kDAKConfig.BetterKnownAs then
 		end
 	end
 	DAKCreateServerAdminCommand("Console_sv_bka", svBka, "<target> <bka> Adds a BKA name to the target.")
-
+	
 	local function EvaluatePlayerName(player, name)
 		name=TrimName(name)
 	    local client = Server.GetOwner(player)
 		if client ~= nil then
 			local steamId = client:GetUserId()
 			local bkaData = LoadBkaData(steamId)
-			if bkaData ~= nil and bkaData.names ~= nil and #bkaData.names > 0 then
-				local bkaName = "[" .. bkaData.names[1] .. "]"
+			if bkaData ~= nil and bkaData.BKA ~= nil and string.len(bkaData.BKA) > 0 then
+				local bkaName = "[" .. bkaData.BKA .. "]"
 				local playerNameStartsWithBkaName = string.sub(name,1,string.len(bkaName))==bkaName
 				if not playerNameStartsWithBkaName then
 					local chatMessage = string.format("Your name must start with '%s' before you play.", bkaName)
