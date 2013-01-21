ab44092d95e4bbfa117abc3937a33ea8ae64e6e5
Peter Kormendi <kormendi@gmail.com>
2013-01-21 00:30:34 -0500

    added sort to scoreboard icons

--- ns2/lua/plugins/config_scoreboardicons.lua
+++ ns2/lua/plugins/config_scoreboardicons.lua
@@ -5,8 +5,10 @@ local function SetupDefaultConfig(Save)
 	if kDAKConfig.ScoreboardIcons == nil then
 		kDAKConfig.ScoreboardIcons = { }
 	end
-		kDAKConfig.ScoreboardIcons.GroupIcons = {}
-		kDAKConfig.ScoreboardIcons.GroupIcons["admin_group"] = "\226\152\133"
+		kDAKConfig.ScoreboardIcons.GroupIcons = {
+				{group = "admin_group", icon = "\226\152\133", sort = 1},
+				{group = "mod_group", icon = "MOD", sort = 2}
+			}
 		kDAKConfig.ScoreboardIcons.CatchAll = "\239\188\159"
 	if Save then
 		SaveDAKConfig()
