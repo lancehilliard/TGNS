6a7afd44310d7419a4958a1bdf3974af8d7c4dd5
M. Lance Hilliard <lance@digitalcreations.cc>
2013-01-31 21:58:32 -0600

    rookiethrottle: refactored to less code

--- ns2/lua/plugins/plugin_rookiethrottle.lua
+++ ns2/lua/plugins/plugin_rookiethrottle.lua
@@ -2,32 +2,19 @@
 
 if kDAKConfig and kDAKConfig.RookieThrottle then
 
-	local function GetPlayerCount() 
-		local playerRecords = Shared.GetEntitiesWithClassname("Player")
-        local result = playerRecords:GetSize()
-		return result
-	end
-
 	local function GetRookieCount()
-		local result = 0
-		local playerList = EntityListToTable(Shared.GetEntitiesWithClassname("Player"))
-		for r = #playerList, 1, -1 do
-			if playerList[r] ~= nil then
-				local playerIsRookie = playerList[r]:GetIsRookie()
-				if playerIsRookie then
-					result = result + 1
-				end
+		local rookieClients = TGNS.GetMatchingClients(TGNS.GetPlayerList(), function(c,p)
+				return p:GetIsRookie()
 			end
-		end
+		)
+		local result = #rookieClients
 		return result
 	end
 	
 	local function RookieThrottleOnClientDelayedConnect(client)
 		local player = TGNS.GetPlayer(client)
-		local playerCount = GetPlayerCount()
-		local rookieCount = GetRookieCount()
 		local playerIsRookie = player:GetIsRookie()
-		if playerCount > kDAKConfig.RookieThrottle.kPlayerCountThreshold and rookieCount > kDAKConfig.RookieThrottle.kRookieCountThreshold and playerIsRookie then
+		if TGNS.GetPlayerCount() > kDAKConfig.RookieThrottle.kPlayerCountThreshold and GetRookieCount() > kDAKConfig.RookieThrottle.kRookieCountThreshold and playerIsRookie then
 			TGNS.SendChatMessage(player, kDAKConfig.RookieThrottle.kPreKickChatMessage)
 			TGNS.KickClient(client, kDAKConfig.RookieThrottle.kKickReason)
 		end
