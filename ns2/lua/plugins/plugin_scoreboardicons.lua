982e283265fbfb226d1dcbb2cdf461d2d5fabeb3
M. Lance Hilliard <lance@digitalcreations.cc>
2013-02-01 01:33:10 -0600

    scoreboardicons: fixed AFK team/admin defect

--- ns2/lua/plugins/plugin_scoreboardicons.lua
+++ ns2/lua/plugins/plugin_scoreboardicons.lua
@@ -3,14 +3,17 @@ if kDAKConfig and kDAKConfig.ScoreboardIcons then
 	Script.Load("lua/TGNSCommon.lua")
 
 	local function PlayerCanSeeAfkStatus(scorePlayer, sendToPlayer)
-		local sendToPlayerCanKickAfkPlayers = TGNS.ClientAction(sendToPlayer, function(c)
-				local playerIsAdmin = TGNS.IsClientAdmin(c)
-				local playerIsTempAdmin = TGNS.IsClientTempAdmin(c)
-				return playerIsAdmin or playerIsTempAdmin
-			end
-		)
-		local sameTeams = TGNS.PlayersAreTeammates(scorePlayer, sendToPlayer)
-		local result = sameTeams or sendToPlayerCanKickAfkPlayers
+		local result = false
+		if scorePlayer ~= nil and sendToPlayer ~= nil then
+		end
+			local sendToPlayerCanKickAfkPlayers = TGNS.ClientAction(sendToPlayer, function(c)
+					local playerIsAdmin = TGNS.IsClientAdmin(c)
+					local playerIsTempAdmin = TGNS.IsClientTempAdmin(c)
+					return playerIsAdmin or playerIsTempAdmin
+				end
+			)
+			local sameTeams = TGNS.PlayersAreTeammates(scorePlayer, sendToPlayer)
+			result = sameTeams or sendToPlayerCanKickAfkPlayers
 		return result
 	end
 	
@@ -29,7 +32,7 @@ if kDAKConfig and kDAKConfig.ScoreboardIcons then
 		local match = false
 		local client = Server.GetOwner(scorePlayer)
 		if client and t and t.playerName then
-			if DAKIsPlayerAFK(scorePlayer) and PlayerCanSeeAfkStatus(sendToPlayer) then
+			if DAKIsPlayerAFK(scorePlayer) and PlayerCanSeeAfkStatus(scorePlayer, sendToPlayer) then
 				t.playerName = prependPlayerName(t.playerName, kDAKConfig.ScoreboardIcons.AFK)
 			else
 				local groupIcons = kDAKConfig.ScoreboardIcons.GroupIcons
