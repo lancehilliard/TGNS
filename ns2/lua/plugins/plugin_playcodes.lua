c5805ebeb8eeca9aaef616148bdb86e0bf08139b
M. Lance Hilliard <lance@digitalcreations.cc>
2013-02-17 01:50:37 -0600

    playcodes: remember successes for a short time

--- ns2/lua/plugins/plugin_playcodes.lua
+++ ns2/lua/plugins/plugin_playcodes.lua
@@ -1,19 +1,41 @@
 // automate voicecomm vetting of strangers
+// Shared.GetSystemTime()
 
 if kDAKConfig and kDAKConfig.PlayCodes then
+
+	local STRANGER_REQUEST_ADVISORY_CHAT = "To keep playing, type 'code?' (in chat) to request a PlayCode from your team!"
+	local STRANGER_LISTEN_CHAT = "Listen to your team for your 3-digit PlayCode, then type it into chat!"
+	local ONCLIENTVETTED_TEAMCHAT_TEMPLATE = "COMMS Established! %s entered the PlayCode!"
+	local ONCLIENTVETTED_STRANGERCHAT = "Do you like teamwork? Become a regular at tacticalgamer.com/natural-selection"
+	local THROTTLE_BETWEEN_VETTINGS_IN_SECONDS = 60
+	local STRANGER_REQUEST_CHAT = "code?"
+	local REGULARS_CHAT_TEMPLATE_SHORT = "%s: %s"
+	local REGULARS_CHAT_TEMPLATE_LONG = "%s needs PlayCode: %s"
+	local REGULARS_REQUIRED_PER_TEAM = 4
+	local NUMBER_OF_SECONDS_TO_REMEMBER_VETTINGS = 3600
+	local NUMBER_OF_SECONDS_TO_WAIT_AFTER_TEAMJOIN_BEFORE_VETTING = 30
+
 	Script.Load("lua/TGNSCommon.lua")
 	Script.Load("lua/TGNSPlayerDataRepository.lua")
 
 	local pdr = TGNSPlayerDataRepository.Create("playcodes", function(data)
 				data.showShorthandCodes = data.showShorthandCodes ~= nil and data.showShorthandCodes or false
+				data.lastVettedOn = data.lastVettedOn ~= nil and data.lastVettedOn or 0
 				return data
-			end
-		)
+			end)
 	
 	local profiles = {}
 	local mapPlayCode = math.random(100, 999)
 	local nextTestAllowedAt = 0
 
+	local function IsTimeToVetClientAgain(client)
+		local steamId = TGNS.GetClientSteamId(client)
+		local data = pdr:Load(steamId)
+		local thirtyMinutesAfterLastVetting = data.lastVettedOn + NUMBER_OF_SECONDS_TO_REMEMBER_VETTINGS
+		local result = Shared.GetSystemTime() >= thirtyMinutesAfterLastVetting
+		return result		
+	end
+	
 	local function MarkAllProfilesInactive(client)
 		local steamId = TGNS.GetClientSteamId(client)
 		TGNS.DoFor(profiles, function(p)
@@ -53,6 +75,9 @@ if kDAKConfig and kDAKConfig.PlayCodes then
 				p.isActive = false
 			end
 		end)
+		local data = pdr:Load(steamId)
+		data.lastVettedOn = Shared.GetSystemTime()
+		pdr:Save(data)
 	end
 	
 	local function MarkProfileAsRequestedByClient(client)
@@ -71,9 +96,9 @@ if kDAKConfig and kDAKConfig.PlayCodes then
 	local function OnClientVetted(client, playCode)
 		MarkAllProfilesInactive(client)
 		MarkProfileAsVettedForClient(client, playCode)
-		TGNS.SendTeamChat(TGNS.PlayerAction(client, TGNS.GetPlayerTeamNumber), string.format("COMMS Established! %s entered the PlayCode!", TGNS.GetClientName(client)), "PlayCodes")
-		TGNS.PlayerAction(client, function(p) TGNS.SendChatMessage(p, "Do you like teamwork? Become a regular at tacticalgamer.com/natural-selection", "PlayCodes") end)
-		nextTestAllowedAt = Shared.GetTime() + 60
+		TGNS.SendTeamChat(TGNS.PlayerAction(client, TGNS.GetPlayerTeamNumber), string.format(ONCLIENTVETTED_TEAMCHAT_TEMPLATE, TGNS.GetClientName(client)), "PlayCodes")
+		TGNS.PlayerAction(client, function(p) TGNS.SendChatMessage(p, ONCLIENTVETTED_STRANGERCHAT, "PlayCodes") end)
+		nextTestAllowedAt = Shared.GetTime() + THROTTLE_BETWEEN_VETTINGS_IN_SECONDS
 	end
 	
 	local function PlayCodeVetsClient(client, playCode)
@@ -95,7 +120,7 @@ if kDAKConfig and kDAKConfig.PlayCodes then
 			if PlayCodeVetsClient(client, playCode) then
 				OnClientVetted(client, message)
 			end
-		elseif message == "code?" then
+		elseif message == STRANGER_REQUEST_CHAT then
 			OnClientRequestedCode(client)
 		end
 	end
@@ -115,14 +140,14 @@ if kDAKConfig and kDAKConfig.PlayCodes then
 			TGNS.DoFor(TGNS.GetPlayers(teamRegularClients), function(p)
 				local steamId = TGNS.ClientAction(p, TGNS.GetClientSteamId)
 				local prefs = pdr:Load(steamId)
-				local teamMessageTemplate = prefs.showShorthandCodes and "%s: %s" or "%s needs PlayCode: %s"
+				local teamMessageTemplate = prefs.showShorthandCodes and REGULARS_CHAT_TEMPLATE_SHORT or REGULARS_CHAT_TEMPLATE_LONG
 				local teamMessage = string.format(teamMessageTemplate, displayName, playCode)
 				TGNS.SendChatMessage(p, teamMessage, "PlayCodes")
 			end)
-			TGNS.PlayerAction(client, function(player) TGNS.SendChatMessage(player, "Listen to your team for your 3-digit PlayCode, then type it into chat!", "PlayCodes") end)
+			TGNS.PlayerAction(client, function(player) TGNS.SendChatMessage(player, STRANGER_LISTEN_CHAT, "PlayCodes") end)
 		else
 			TGNS.PlayerAction(client, function(p)
-				TGNS.SendChatMessage(p, "To keep playing, type 'code?' (in chat) to request a PlayCode from your team!", "PlayCodes")
+				TGNS.SendChatMessage(p, STRANGER_REQUEST_ADVISORY_CHAT, "PlayCodes")
 			end)
 		end
 	end
@@ -148,7 +173,7 @@ if kDAKConfig and kDAKConfig.PlayCodes then
 							end)
 							if clientIsOnGameplayTeam and clientIsActive and p.doNotEnforceBefore < Shared.GetTime() then
 								local teamRegularClients = GetTeamRegularClients(client)
-								if #teamRegularClients >= 4 then
+								if #teamRegularClients >= REGULARS_REQUIRED_PER_TEAM then
 									if p.noticesRemaining > 0 then
 										p.noticesRemaining = p.noticesRemaining - 1
 										SendChatMessages(client, p.playCode)
@@ -177,13 +202,16 @@ if kDAKConfig and kDAKConfig.PlayCodes then
 		result.noticesRemaining = 12
 		result.playCode = mapPlayCode
 		result.steamId = TGNS.GetClientSteamId(client)
-		result.doNotEnforceBefore = Shared.GetTime() + 30
+		result.doNotEnforceBefore = Shared.GetTime() + NUMBER_OF_SECONDS_TO_WAIT_AFTER_TEAMJOIN_BEFORE_VETTING
+		
+		TGNS.SendAdminChat(string.format("Profile created for %s", result.steamId))
+		
 		return result
 	end
 	
 	local function PlayCodesOnTeamJoin(self, player, newTeamNumber, force)
 		TGNS.ClientAction(player, function(c)
-			if TGNS.IsClientStranger(c) then
+			if TGNS.IsClientAdmin(c) and IsTimeToVetClientAgain(c) then
 				MarkAllProfilesInactive(c)
 				if not HasClientBeenVetted(c) then
 					local profile = CreateProfile(c)
