67bb81d856548b26e2497b1b246dc34b44cd3885
M. Lance Hilliard <lance@digitalcreations.cc>
2013-02-18 00:19:00 -0600

    speclimit: added spectators to spectator_group
    spectator_group includes sv_afkimmune on TGNS

--- ns2/lua/plugins/plugin_speclimit.lua
+++ ns2/lua/plugins/plugin_speclimit.lua
@@ -4,10 +4,22 @@ if kDAKConfig and kDAKConfig.SpecLimit then
 	Script.Load("lua/TGNSCommon.lua")
 	local function SpecLimitOnTeamJoin(self, player, newTeamNumber, force)
 		local cancel = false
+		if TGNS.IsPlayerSpectator(player) then
+			local client = TGNS.ClientAction(player, function(c) return c end)
+			local steamId = TGNS.GetClientSteamId(client)
+			RemoveSteamIDFromGroup(steamId, "spectator_group")
+		end
 		local playerIsAdmin = TGNS.ClientAction(player, TGNS.IsClientAdmin)
-		if newTeamNumber == kSpectatorIndex and not playerIsAdmin then
-			TGNS.SendChatMessage(player, "Spectator is not available now.")
-			cancel = true
+		if newTeamNumber == kSpectatorIndex then
+			if not playerIsAdmin then
+				TGNS.SendChatMessage(player, "Spectator is not available now.")
+				cancel = true
+			end
+			if not cancel then
+				local client = TGNS.ClientAction(player, function(c) return c end)
+				local steamId = TGNS.GetClientSteamId(client)
+				AddSteamIDToGroup(TGNS.GetClientSteamId(client), "spectator_group")
+			end
 		end
 		return cancel
 	end
