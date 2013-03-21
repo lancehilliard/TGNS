82e22b623dda21f74e0815ecdf8e85d2251f4188
M. Lance Hilliard <lance@digitalcreations.cc>
2013-03-21 01:16:23 -0500

    groundedrookies: enforced when 4+ non-rookies

--- ns2/lua/plugins/groundedrookies.lua
+++ ns2/lua/plugins/groundedrookies.lua
@@ -5,7 +5,7 @@ local notifiedPlayerIds = {}
 local originalNS2GRGetPlayerBannedFromCommand = Class_ReplaceMethod(DAK.config.loader.GamerulesClassName, "GetPlayerBannedFromCommand", function(self, playerId)
 	local player = DAK:GetPlayerMatching(playerId)
 	local numberOfNonRookiesOnTeam = #TGNS.GetMatchingClients(TGNS.GetPlayerList(), function(c,p) return TGNS.PlayersAreTeammates(player, p) and not TGNS.PlayerIsRookie(p) end)
-	local cancel = TGNS.PlayerIsRookie(player) and numberOfNonRookiesOnTeam >= 0
+	local cancel = TGNS.PlayerIsRookie(player) and numberOfNonRookiesOnTeam >= 4
 	if cancel then
 		if not TGNS.Has(notifiedPlayerIds, playerId) then
 			TGNS.SendTeamChat(TGNS.GetPlayerTeamNumber(player), string.format("%s! Rookies will fight from the ground this game. Help them!", TGNS.GetPlayerTeamName(player)), "TGNS")
