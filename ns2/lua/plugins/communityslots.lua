a4f86818b2fbc6938bd43f85b1ff706bc59a27c0
M. Lance Hilliard <lance@digitalcreations.cc>
2013-04-05 19:23:23 -0500

    added: teamres plugin
    refactored teamres functionality out of communityslots plugin

--- ns2/lua/plugins/communityslots.lua
+++ ns2/lua/plugins/communityslots.lua
@@ -9,17 +9,6 @@ local MESSAGE_PREFIX = "SLOTS"
 table.insert(actionslog, "COMMUNITY SLOTS DEBUG: ")
 local victimBumpCounts = {}
 local rejectBumpCounts = {}
-local abandonedResources
-local lastKnownGameplayTeamNumber
-
-local function ResetAbandonedResources()
-	abandonedResources = {
-      [1] = {},
-      [2] = {}
-    }
-	lastKnownGameplayTeamNumber = {}
-end
-ResetAbandonedResources()
 
 local function IsTargetProtectedStranger(targetClient, playerList)
 	local result = TGNS.IsClientStranger(targetClient) and #TGNS.GetStrangersClients(playerList) <= DAK.config.communityslots.kMinimumStrangers
@@ -160,28 +149,6 @@ local function GetBumpSummary(playerList, bumpedClient, joinerOrVictim)
 	return result
 end
 
-local function AnnounceAbandonedResources(client, resources)
-	local debugMessage = string.format("%s abandoned %s resources.", TGNS.GetClientName(client), math.floor(resources))
-	if math.floor(resources) > 0 then
-		TGNS.SendAdminConsoles(debugMessage, "TEAMRESDEBUG")
-	end
-end
-
-local function AbandonResources(client)
-	if TGNS.IsGameInProgress() then
-		local player = TGNS.GetPlayer(client)
-		local playerTeamNumber = TGNS.GetPlayerTeamNumber(player)
-		if TGNS.IsGameplayTeam(playerTeamNumber) then
-			if abandonedResources[playerTeamNumber][client] == nil then
-				local resources = TGNS.GetPlayerClassPurchaseCost(player) + TGNS.GetMarineWeaponsTotalPurchaseCost(player) + TGNS.GetPlayerResources(player)
-				abandonedResources[playerTeamNumber][client] = resources
-				TGNS.SetPlayerResources(player, 0)
-				AnnounceAbandonedResources(client, resources)
-			end
-		end
-	end
-end
-
 local function IsClientBumped(client)
 	local result = false
 	local playerList = GetFullyConnectedPlayers(client)
@@ -197,8 +164,10 @@ local function IsClientBumped(client)
 			result = true
 		end
 	end
-	if not result then
-		table.insert(clientsWhoAreConnectedEnoughToBeConsideredBumpable, client)
+	if result then
+		TGNS.RemoveAllMatching(clientsWhoAreConnectedEnoughToBeConsideredBumpable, client)
+	else
+		table.insertunique(clientsWhoAreConnectedEnoughToBeConsideredBumpable, client)
 	end
 	return result
 end
@@ -253,7 +222,6 @@ local function OnGameEnd()
 			)
 		end
 	)
-	ResetAbandonedResources()
 end
 TGNS.RegisterEventHook("OnGameEnd", function() TGNS.ScheduleAction(8, OnGameEnd) end)
 
@@ -279,70 +247,9 @@ local function PrintPlayerSlotsStatuses(client)
 end
 TGNS.RegisterCommandHook("Console_sv_csinfo", PrintPlayerSlotsStatuses, "Print Community Slots bump counts and player statuses.", true)
 
-local function AnnounceReceivedResources(player, resources)
-	local message = string.format("%s got %s resources from a departed teammate.", TGNS.GetPlayerName(player), math.floor(resources))
-	if math.floor(resources) > 0 then
-		TGNS.SendChatMessage(player, message, "TEAMRES")
-		TGNS.SendAdminConsoles(message, "TEAMRESDEBUG")
-	end
-end
-
-local function DistributeAbandonedResources(client, teamNumber)
-	local player = TGNS.GetPlayer(client)
-	local playerTeamNumber = TGNS.GetPlayerTeamNumber(player)
-	if TGNS.IsGameplayTeam(playerTeamNumber) then
-		if playerTeamNumber == teamNumber then
-			local resKey
-			local giveableResources
-			if lastKnownGameplayTeamNumber[client] == teamNumber then
-				resKey = client
-				giveableResources = abandonedResources[playerTeamNumber][client]
-			else
-				TGNS.DoForPairs(abandonedResources[playerTeamNumber], function(key, resources)
-					if resources ~= nil and (giveableResources == nil or resources > giveableResources) then
-						giveableResources = resources
-						resKey = key
-					end
-				end)
-			end
-			if giveableResources ~= nil then
-				local originalResources = TGNS.GetPlayerResources(player)
-				if math.floor(giveableResources) > math.floor(originalResources) then
-					TGNS.SetPlayerResources(player, giveableResources)
-					abandonedResources[playerTeamNumber][resKey] = math.floor(originalResources) > 0 and originalResources or nil
-					AnnounceAbandonedResources(client, originalResources)
-					AnnounceReceivedResources(player, giveableResources)
-				end
-			end
-			lastKnownGameplayTeamNumber[client] = teamNumber
-		end
-	end
-end
-
 local function CommunitySlotsOnTeamJoin(self, player, newTeamNumber, force)
-	local cancel = false
 	local joiningClient = TGNS.GetClient(player)
-	local playerIsDroppingToReadyRoom = TGNS.IsGameplayTeam(TGNS.GetPlayerTeamNumber(player)) and newTeamNumber == kTeamReadyRoom
-	local playerIsJoiningTeam = TGNS.IsGameplayTeam(newTeamNumber)
-	if playerIsDroppingToReadyRoom then
-		AbandonResources(joiningClient)
-	elseif playerIsJoiningTeam then
-		cancel = IsClientBumped(joiningClient)
-		if not cancel then
-			TGNS.ScheduleAction(2, function() DistributeAbandonedResources(joiningClient, newTeamNumber) end)
-		end
-	end
+	local cancel = IsClientBumped(joiningClient)
 	return cancel
 end
-TGNS.RegisterEventHook("OnTeamJoin", CommunitySlotsOnTeamJoin, TGNS.LOWEST_EVENT_HANDLER_PRIORITY)
-
-local function ShowPlayerCosts(client)
-	TGNS.DoFor(TGNS.GetPlayerList(), function(p)
-		local name = TGNS.GetPlayerName(p)
-		local className = TGNS.GetPlayerClassName(p)
-		local classPurchaseCost = TGNS.GetPlayerClassPurchaseCost(p)
-		local weaponsCost = TGNS.GetMarineWeaponsTotalPurchaseCost(p)
-		TGNS.ConsolePrint(client, string.format("%s: %s %s %s", name, className, classPurchaseCost, weaponsCost), "COSTS")
-	end)
-end
-TGNS.RegisterCommandHook("Console_sv_showcosts", ShowPlayerCosts, "Print costs of all players.")
+TGNS.RegisterEventHook("OnTeamJoin", CommunitySlotsOnTeamJoin)
