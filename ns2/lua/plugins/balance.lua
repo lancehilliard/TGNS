56c5f551050d15449fa4ef94eb6938180fe7ea9a
M. Lance Hilliard <lance@digitalcreations.cc>
2013-06-18 23:58:37 -0500

    balance: score-per-min, team lock, hide scorebrd

--- ns2/lua/plugins/balance.lua
+++ ns2/lua/plugins/balance.lua
@@ -1,13 +1,21 @@
 Script.Load("lua/TGNSCommon.lua")
 Script.Load("lua/TGNSPlayerDataRepository.lua")
+Script.Load("lua/TGNSNs2StatsProxy.lua")
+Script.Load("lua/TGNSAverageCalculator.lua")
+Script.Load("lua/TGNSScoreboardPlayerHider.lua")
 local steamIdsWhichStartedGame = {}
 local balanceLog = {}
 local balanceInProgress = false
+local lastBalanceStartTimeInSeconds = 0
+local SCORE_PER_MINUTE_DATAPOINTS_TO_KEEP = 30
+local ns2statsProxy
+local RECENT_BALANCE_DURATION_IN_SECONDS = 15
 
 local pdr = TGNSPlayerDataRepository.Create("balance", function(balance)
 			balance.wins = balance.wins ~= nil and balance.wins or 0
 			balance.losses = balance.losses ~= nil and balance.losses or 0
 			balance.total = balance.total ~= nil and balance.total or 0
+			balance.scoresPerMinute = balance.scoresPerMinute ~= nil and balance.scoresPerMinute or {}
 			return balance
 		end
 	)
@@ -29,7 +37,6 @@ local addWinToBalance = function(balance)
 		if balance.wins + balance.losses > 100 then
 			balance.losses = balance.losses - 1
 		end
-		//Shared.Message(balance.steamId .. " WIN")
 	end
 local addLossToBalance = function(balance) 
 		balance.losses = balance.losses + 1 
@@ -37,9 +44,24 @@ local addLossToBalance = function(balance)
 		if balance.wins + balance.losses > 100 then
 			balance.wins = balance.wins - 1
 		end
-		//Shared.Message(balance.steamId .. " LOSS")
 	end
 
+local function BalanceStartedRecently()
+	local result = Shared.GetTime() > RECENT_BALANCE_DURATION_IN_SECONDS and Shared.GetTime() - lastBalanceStartTimeInSeconds < RECENT_BALANCE_DURATION_IN_SECONDS
+	return result
+end
+	
+local function AddScorePerMinuteData(balance, scorePerMinute)
+	table.insert(balance.scoresPerMinute, scorePerMinute)
+	local scoresPerMinuteToKeep = {}
+	TGNS.DoForReverse(balance.scoresPerMinute, function(scorePerMinute)
+		if #scoresPerMinuteToKeep < SCORE_PER_MINUTE_DATAPOINTS_TO_KEEP then
+			table.insert(scoresPerMinuteToKeep, scorePerMinute)
+		end
+	end)
+	balance.scoresPerMinute = scoresPerMinuteToKeep
+end
+	
 local function GetWinLossRatio(player, balance)
 	local result = 0.5
 	if balance ~= nil then
@@ -64,27 +86,48 @@ local function GetPlayerBalance(player)
 	return result
 end
 
+local function UpdateNs2StatsProxyWithRecordsOfPlayersWhoHaveTooFewLocalScoresPerMinute()
+	local playersWithTooFewLocalScoresPerMinute = TGNS.Where(TGNS.GetPlayerList(), function(p) return #GetPlayerBalance(p).scoresPerMinute < 10 end)
+	local steamIdsWithTooFewLocalScoresPerMinute = TGNS.Select(playersWithTooFewLocalScoresPerMinute, function(p) return TGNS.ClientAction(p, TGNS.GetClientSteamId) end)
+	ns2statsProxy = TGNSNs2StatsProxy.Create(steamIdsWithTooFewLocalScoresPerMinute)
+end
+	
+local function GetPlayerScorePerMinuteAverage(player)
+	local balance = GetPlayerBalance(player)
+	local result = #balance.scoresPerMinute < 10 and nil or TGNSAverageCalculator.CalculateFor(balance.scoresPerMinute)
+	if result == nil and ns2statsProxy ~= nil then
+		local steamId = TGNS.ClientAction(player, TGNS.GetClientSteamId)
+		local ns2StatsPlayerRecord = ns2statsProxy.GetPlayerRecord(steamId)
+		if ns2StatsPlayerRecord.HasData then
+			local cumulativeScore = ns2StatsPlayerRecord.GetCumulativeScore()
+			local timePlayedInMinutes = TGNS.ConvertSecondsToMinutes(ns2StatsPlayerRecord.GetTimePlayedInSeconds())
+			result = TGNSAverageCalculator.Calculate(cumulativeScore, timePlayedInMinutes)
+		end
+	end
+	return result or 0
+end
+
 local function GetPlayerWinLossRatio(player)
 	local balance = GetPlayerBalance(player)
 	local result = GetWinLossRatio(player, balance)
 	return result
 end
 
-local function ChangeBalance(steamId, changeAction)
-	local balance = pdr:Load(steamId)
-	changeAction(balance)
-	pdr:Save(balance)
+local function GetPlayerProjectionAverage(clients, playerProjector)
+	local values = TGNS.Select(clients, function(c)
+		return TGNS.PlayerAction(c, playerProjector)
+	end)
+	local result = TGNSAverageCalculator.CalculateFor(values)
+	return result
+end
+
+local function GetScorePerMinuteAverage(clients)
+	local result = GetPlayerProjectionAverage(clients, GetPlayerScorePerMinuteAverage) or 0
+	return result
 end
 
 local function GetWinLossAverage(clients)
-	local result = 0
-	if #clients > 0 then
-		local total = 0
-		TGNS.DoFor(clients, function(c)
-			total = total + TGNS.PlayerAction(c, function(p) return GetPlayerWinLossRatio(p) end)
-		end)
-		result = total / #clients
-	end
+	local result = GetPlayerProjectionAverage(clients, GetPlayerWinLossRatio) or 0
 	return result
 end
 
@@ -95,25 +138,44 @@ local function PrintBalanceLog()
 end
 
 local function SendNextPlayer()
-	local playerList = TGNS.GetPlayerList()
-	local playersWithFewerThanTenGames = TGNS.GetPlayers(TGNS.GetMatchingClients(playerList, function(c,p) return GetPlayerBalance(p).total < 10 end))
-	local playersWithTenOrMoreGames = TGNS.GetPlayers(TGNS.GetMatchingClients(playerList, function(c,p) return GetPlayerBalance(p).total >= 10 end))
-	table.sort(playersWithTenOrMoreGames, function(p1, p2) return GetPlayerWinLossRatio(p1) > GetPlayerWinLossRatio(p2) end )
-	local allPlayers = playersWithFewerThanTenGames
-	TGNS.DoFor(playersWithTenOrMoreGames, function(p)
-		table.insert(allPlayers, p)
-	end)
-	local readyRoomClient = TGNS.GetLastMatchingClient(allPlayers, function(c,p) return TGNS.IsPlayerReadyRoom(p) and not TGNS.IsPlayerAFK(p) end)
+	local wantToUseWinLossToBalance = false
+	
+	local playersBuilder
+	local teamAverageGetter
+	
+	if wantToUseWinLossToBalance then
+		playersBuilder = function(playerList)
+			local playersWithFewerThanTenGames = TGNS.GetPlayers(TGNS.GetMatchingClients(playerList, function(c,p) return GetPlayerBalance(p).total < 10 end))
+			local playersWithTenOrMoreGames = TGNS.GetPlayers(TGNS.GetMatchingClients(playerList, function(c,p) return GetPlayerBalance(p).total >= 10 end))
+			TGNS.SortDescending(playersWithTenOrMoreGames, GetPlayerWinLossRatio)
+			local result = playersWithFewerThanTenGames
+			TGNS.DoFor(playersWithTenOrMoreGames, function(p)
+				table.insert(result, p)
+			end)
+			return result
+		end
+		teamAverageGetter = GetWinLossAverage
+	else
+		playersBuilder = function(playerList)
+			local result = playerList
+			TGNS.SortDescending(result, GetPlayerScorePerMinuteAverage)
+			return result
+		end
+		teamAverageGetter = GetScorePerMinuteAverage
+	end
+
+	local players = playersBuilder(TGNS.GetPlayerList())
+	local readyRoomClient = TGNS.GetLastMatchingClient(players, function(c,p) return TGNS.IsPlayerReadyRoom(p) and not TGNS.IsPlayerAFK(p) end)
 	if readyRoomClient then
 		local player = TGNS.GetPlayer(readyRoomClient)
 		local teamNumber = nil
 		local actionMessage
 
-		local updatedPlayerList = TGNS.GetPlayerList()
-		local marineClients = TGNS.GetMarineClients(updatedPlayerList)
-		local alienClients = TGNS.GetAlienClients(updatedPlayerList)
-		local marineAvg = GetWinLossAverage(marineClients)
-		local alienAvg = GetWinLossAverage(alienClients)
+		local playerList = TGNS.GetPlayerList()
+		local marineClients = TGNS.GetMarineClients(playerList)
+		local alienClients = TGNS.GetAlienClients(playerList)
+		local marineAvg = teamAverageGetter(marineClients)
+		local alienAvg = teamAverageGetter(alienClients)
 		local marineCount = #marineClients
 		local alienCount = #alienClients
 		if marineAvg <= alienAvg then
@@ -122,8 +184,7 @@ local function SendNextPlayer()
 			teamNumber = alienCount <= marineCount and kAlienTeamType or kMarineTeamType
 		end
 		actionMessage = string.format("sent to %s", TGNS.GetTeamName(teamNumber))
-
-		table.insert(balanceLog, string.format("%s: %s with %s = %s", player:GetName(), GetPlayerWinLossRatio(player), GetPlayerBalance(player).total, actionMessage))
+		table.insert(balanceLog, string.format("%s: %s with %s = %s", player:GetName(), GetPlayerScorePerMinuteAverage(player), GetPlayerBalance(player).total, actionMessage))
 		TGNS.SendToTeam(player, teamNumber)
 		TGNS.ScheduleAction(0.25, SendNextPlayer)
 	else
@@ -131,11 +192,11 @@ local function SendNextPlayer()
 		TGNS.SendAdminConsoles("Balance finished.", "ADMINDEBUG")
 		Shared.Message("Balance finished.")
 		balanceInProgress = false
-		playerList = TGNS.GetPlayerList()
-		marineClients = TGNS.GetMarineClients(playerList)
-		alienClients = TGNS.GetAlienClients(playerList)
-		marineAvg = GetWinLossAverage(marineClients)
-		alienAvg = GetWinLossAverage(alienClients)
+		local playerList = TGNS.GetPlayerList()
+		local marineClients = TGNS.GetMarineClients(playerList)
+		local alienClients = TGNS.GetAlienClients(playerList)
+		local marineAvg = teamAverageGetter(marineClients)
+		local alienAvg = teamAverageGetter(alienClients)
 		local averagesReport = string.format("MarineAvg: %s | AlienAvg: %s", marineAvg, alienAvg)
 		table.insert(balanceLog, averagesReport)
 		TGNS.ScheduleAction(1, PrintBalanceLog)
@@ -149,11 +210,14 @@ end
 
 local function svBalance(client)
 	local gameState = GetGamerules():GetGameState()
-	local whenDescriptor
 	if gameState == kGameState.NotStarted or gameState == kGameState.PreGame then
-		TGNS.SendAllChat(string.format("%s is balancing teams using TG Win/Loss histories.", TGNS.GetClientName(client)), "TacticalGamer.com")
+		TGNS.SendAllChat(string.format("%s is balancing teams using TG and ns2stats score-per-minute data.", TGNS.GetClientName(client)), "TacticalGamer.com")
 		balanceInProgress = true
+		lastBalanceStartTimeInSeconds = Shared.GetTime()
+		TGNS.ScheduleAction(1, UpdateNs2StatsProxyWithRecordsOfPlayersWhoHaveTooFewLocalScoresPerMinute)
 		TGNS.ScheduleAction(5, BeginBalance)
+		TGNS.ScheduleAction(RECENT_BALANCE_DURATION_IN_SECONDS + 1, TGNS.UpdateAllScoreboards)
+		TGNS.UpdateAllScoreboards()
 	end
 end
 TGNS.RegisterCommandHook("Console_sv_balance", svBalance, "Balances all players based on TG win/loss (percentage) record.")
@@ -171,10 +235,31 @@ TGNS.RegisterEventHook("OnSetGameState", BalanceOnSetGameState)
 local function BalanceOnGameEnd(self, winningTeam)
 	TGNS.DoForClientsWithId(TGNS.GetPlayingClients(TGNS.GetPlayerList()), function(c, steamId)
 			if TGNS.Has(steamIdsWhichStartedGame, steamId) then
-				local changeBalanceFunction = TGNS.PlayerIsOnTeam(TGNS.GetPlayer(c), winningTeam) and addWinToBalance or addLossToBalance
-				ChangeBalance(steamId, changeBalanceFunction)
+				local player = TGNS.GetPlayer(c)
+				local changeBalanceFunction = TGNS.PlayerIsOnTeam(player, winningTeam) and addWinToBalance or addLossToBalance
+				local balance = pdr:Load(steamId)
+				changeBalanceFunction(balance)
+				AddScorePerMinuteData(balance, TGNS.GetPlayerScorePerMinute(player))
+				pdr:Save(balance)
 			end
 		end
 	)
 end
 TGNS.RegisterEventHook("OnGameEnd", BalanceOnGameEnd)
+
+TGNS.RegisterEventHook("OnTeamJoin", function(self, player, newTeamNumber, force)
+	if BalanceStartedRecently() then
+		TGNS.UpdateAllScoreboards()
+	end
+	local cancel = false
+	cancel = BalanceStartedRecently() and TGNS.PlayerIsOnPlayingTeam(player) and not TGNS.ClientAction(player, TGNS.IsClientAdmin)
+	if cancel then
+		local message = string.format("%s may not switch teams within %s seconds of Balance.", TGNS.GetPlayerName(player), RECENT_BALANCE_DURATION_IN_SECONDS)
+		TGNS.SendAllChat(message, "BALANCE")
+	end
+	return cancel
+end)
+
+TGNSScoreboardPlayerHider.RegisterHidingPredicate(function(targetPlayer, message)
+	return BalanceStartedRecently() and not TGNS.PlayerIsOnPlayingTeam(targetPlayer)
+end)
