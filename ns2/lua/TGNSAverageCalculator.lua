5113643fa6ac31380a9140a2164b17b26b7cae81
M. Lance Hilliard <lance@digitalcreations.cc>
2013-06-18 23:57:44 -0500

    AverageCalculator: added CalculateFor

--- ns2/lua/TGNSAverageCalculator.lua
+++ ns2/lua/TGNSAverageCalculator.lua
@@ -1,5 +1,17 @@
+Script.Load("lua/TGNSCommon.lua")
+
 TGNSAverageCalculator = {}
 TGNSAverageCalculator.Calculate = function(total, divisor)
 	local result = total / divisor
+	return result
+end
+
+TGNSAverageCalculator.CalculateFor = function(numbers)
+	local result
+	if numbers ~= nil and #numbers > 0 then
+		local total = 0
+		TGNS.DoFor(numbers, function(n) total = total + n end)
+		result = TGNSAverageCalculator.Calculate(total, #numbers)
+	end
 	return result
 end
