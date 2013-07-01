69948cc4c98918de2af034e11fe38197f8bea29e
M. Lance Hilliard <lance@digitalcreations.cc>
2013-06-30 21:24:36 -0500

    ClientKicker: refactor: use ScoreboardPlayerHider

--- ns2/lua/TGNSClientKicker.lua
+++ ns2/lua/TGNSClientKicker.lua
@@ -1,4 +1,5 @@
 Script.Load("lua/TGNSCommon.lua")
+Script.Load("lua/TGNSScoreboardPlayerHider.lua")
 
 TGNSClientKicker = {}
 local kickedClients = {}
@@ -69,27 +70,4 @@ local function onChatClient(client, networkMessage)
 end
 TGNS.RegisterNetworkMessageHook("ChatClient", onChatClient, 5)
 
-local originalSendNetworkMessage = Server.SendNetworkMessage
-
-Server.SendNetworkMessage = function(arg1, arg2, arg3, ...)
-	local networkMessage = ""
-	local message
-	local target
-	if type(arg1) == "string" then
-		networkMessage = arg1
-		message = arg2
-	elseif type(arg2) == "string" then
-		target = arg1
-		networkMessage = arg2
-		message = arg3
-	end
-	
-	if networkMessage == "Scores" and message then
-		if target and TGNSClientKicker.IsClientKicked(target) then
-			Server.SendCommand(target, string.format("clientdisconnect %d", message.clientId) )
-			return
-		end
-	end
-	
-	originalSendNetworkMessage(arg1, arg2, arg3, ...)
-end
+TGNSScoreboardPlayerHider.RegisterHidingPredicate(TGNSClientKicker.IsClientKicked)
