dc43bab0ca8e0d10130b22570b6deb684e7a8337
Richard <snooggums@gmail.com>
2013-07-01 17:39:19 -0500

    some more changes for tempban, still untested

--- ns2/lua/plugins/tempban.lua
+++ ns2/lua/plugins/tempban.lua
@@ -2,14 +2,16 @@ Script.Load("lua/TGNSCommon.lua")
 
 //Still need to add to list of denied and allowed functions by type
 function svTempBan(client, playerName, ...)
-	local targetPlayer = TGNS.GetPlayerMatching(playerName, nil)
-	if targetPlayer ~= nil then
-			DisplayMessageAll("svtempban was initiated")
-			TGNS.GetPlayerId(targetPlayer)	
-			TGNS.Ban(client, playerId, 15, ...)
-	else
-		TGNS.SendAdminChat("svTempban initiated no target found", "ADMINDEBUG")
-	end
+	TGNS.SendAdminChat("svTempban initiated", "ADMINDEBUG")
+//	local player = TGNS.GetPlayerMatching(id, nil)
+//	local reason =  StringConcatArgs(...) or "No Reason"
+//	if player ~= nil then
+//			local playerId = TGNS.GetPlayerId(player)	
+//			TGNS.Ban(client, playerId, 15, ...)
+//			TGNS.SendAdminChat(player:GetName() .. " has been banned.", "ADMINDEBUG")
+//	else
+//		TGNS.SendAdminChat("svTempban initiated no target found", "ADMINDEBUG")
+//	end
 end
 TGNS.RegisterCommandHook("Console_sv_tempban", svTempBan, "<player> <reason> Bans players for 15 minutes.")
 
