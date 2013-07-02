22d8117d73fcdd180602d2f465d55eedf6cf03bd
M. Lance Hilliard <lance@digitalcreations.cc>
2013-07-01 23:35:26 -0500

    tempban: second draft

--- ns2/lua/plugins/tempban.lua
+++ ns2/lua/plugins/tempban.lua
@@ -1,16 +1,37 @@
 Script.Load("lua/TGNSCommon.lua")
+Script.Load("lua/TGNSMessageDisplayer.lua")
+Script.Load("lua/TGNSClientKicker.lua")
 
-//Still need to add to list of denied and allowed functions by type
-function svTempBan(client, playerName, ...)
-	TGNS.SendAdminChat("svTempban initiated", "ADMINDEBUG")
-//	local player = TGNS.GetPlayerMatching(id, nil)
-//	local reason =  StringConcatArgs(...) or "No Reason"
-//	if player ~= nil then
-//			local playerId = TGNS.GetPlayerId(player)	
-//			TGNS.Ban(client, playerId, 15, ...)
-//			TGNS.SendAdminChat(player:GetName() .. " has been banned.", "ADMINDEBUG")
-//	else
-//		TGNS.SendAdminChat("svTempban initiated no target found", "ADMINDEBUG")
-//	end
+local tempbanMessageDisplayer = TGNSMessageDisplayer.Create("TEMPBAN")
+
+local TEMPBAN_DURATION_IN_MINUTES = 15
+
+local function ShowUsage(client)
+	tempbanMessageDisplayer:ToClientConsole(client, "Usage: sv_tempban <player> <reason>")
+end
+
+local function svTempBan(client, playerName, ...)
+	if TGNS.IsClientAdmin(client) or TGNS.IsClientTempAdmin(client) then
+		local targetPlayer = TGNS.GetPlayerMatching(playerName, nil)
+		if targetPlayer ~= nil then
+			local reason = TGNS.GetConcatenatedStringOrEmpty(...)
+			if reason == "" then
+				tempbanMessageDisplayer:ToClientConsole(client, string.format("You must specify a reason to tempban '%s'.", TGNS.GetPlayerName(targetPlayer)))
+				ShowUsage(client)
+			else
+				local targetClient = TGNS.GetClient(targetPlayer)
+				TGNS.Ban(targetClient, TEMPBAN_DURATION_IN_MINUTES, reason)
+				TGNSClientKicker.Kick(targetClient, reason)
+				local message = string.format("%s banned %s for %s minutes.", TGNS.GetClientName(client), TGNS.GetClientName(targetClient), TEMPBAN_DURATION_IN_MINUTES)
+				tempbanMessageDisplayer:ToAdminChat(message)
+				tempbanMessageDisplayer:ToClientConsole(client, message)
+			end
+		else
+			tempbanMessageDisplayer:ToClientConsole(client, string.format("'%s' does not uniquely match a player.", playerName))
+			ShowUsage(client)
+		end
+	else
+		tempbanMessageDisplayer:ToClientConsole(client, "You must be a Temp Admin or Admin to use this command.")
+	end
 end
 TGNS.RegisterCommandHook("Console_sv_tempban", svTempBan, "<player> <reason> Bans players for 15 minutes.")
