123dd8efb52b0e05a2f9f31992b5d7c2ac4a4ac7
M. Lance Hilliard <lance@digitalcreations.cc>
2013-07-04 00:30:59 -0500

    tempban: third draft

--- ns2/lua/plugins/tempban.lua
+++ ns2/lua/plugins/tempban.lua
@@ -8,27 +8,34 @@ local TEMPBAN_DURATION_IN_MINUTES = 15
 
 local function ShowUsage(client)
 	tempbanMessageDisplayer:ToClientConsole(client, "Usage: sv_tempban <player> <reason>")
+	tempbanMessageDisplayer:ToClientConsole(client, "       All usages logged. Use responsibly!")
 end
 
 local function svTempBan(client, playerName, ...)
 	if TGNS.IsClientAdmin(client) or TGNS.IsClientTempAdmin(client) then
-		local targetPlayer = TGNS.GetPlayerMatching(playerName, nil)
-		if targetPlayer ~= nil then
-			local reason = TGNS.GetConcatenatedStringOrEmpty(...)
-			if reason == "" then
-				tempbanMessageDisplayer:ToClientConsole(client, string.format("You must specify a reason to tempban '%s'.", TGNS.GetPlayerName(targetPlayer)))
-				ShowUsage(client)
-			else
-				local targetClient = TGNS.GetClient(targetPlayer)
-				TGNS.Ban(targetClient, TEMPBAN_DURATION_IN_MINUTES, reason)
-				TGNSClientKicker.Kick(targetClient, reason)
-				local message = string.format("%s banned %s for %s minutes.", TGNS.GetClientName(client), TGNS.GetClientName(targetClient), TEMPBAN_DURATION_IN_MINUTES)
-				tempbanMessageDisplayer:ToAdminChat(message)
-				tempbanMessageDisplayer:ToClientConsole(client, message)
-			end
-		else
-			tempbanMessageDisplayer:ToClientConsole(client, string.format("'%s' does not uniquely match a player.", playerName))
+		if playerName == nil or playerName == "" then
+			tempbanMessageDisplayer:ToClientConsole(client, "You must specify a player.")
 			ShowUsage(client)
+		else
+			local targetPlayer = TGNS.GetPlayerMatching(playerName, nil)
+			if targetPlayer ~= nil then
+				local reason = TGNS.GetConcatenatedStringOrEmpty(...)
+				if reason == "" then
+					tempbanMessageDisplayer:ToClientConsole(client, string.format("You must specify a reason to tempban '%s'.", TGNS.GetPlayerName(targetPlayer)))
+					ShowUsage(client)
+				else
+					local targetClient = TGNS.GetClient(targetPlayer)
+					TGNS.Ban(targetClient, TEMPBAN_DURATION_IN_MINUTES, reason)
+					TGNSClientKicker.Kick(targetClient, reason)
+					local message = string.format("%s banned %s for %s minutes.", TGNS.GetClientName(client), TGNS.GetClientName(targetClient), TEMPBAN_DURATION_IN_MINUTES)
+					tempbanMessageDisplayer:ToAdminChat(message)
+					tempbanMessageDisplayer:ToClientConsole(client, message)
+					TGNS.EnhancedLog(string.format("%s executed tempban against %s with reason '%s'.", TGNS.GetClientNameSteamIdCombo(client), TGNS.GetClientNameSteamIdCombo(targetClient), reason))
+				end
+			else
+				tempbanMessageDisplayer:ToClientConsole(client, string.format("'%s' does not uniquely match a player.", playerName))
+				ShowUsage(client)
+			end
 		end
 	else
 		tempbanMessageDisplayer:ToClientConsole(client, "You must be a Temp Admin or Admin to use this command.")
