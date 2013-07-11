72d507abdc11ab976bb4b65c4eb26605c8855de6
M. Lance Hilliard <lance@digitalcreations.cc>
2013-07-11 01:22:18 -0500

    groundedrookies: pregame and team dead

--- ns2/lua/plugins/groundedrookies.lua
+++ ns2/lua/plugins/groundedrookies.lua
@@ -2,16 +2,21 @@ Script.Load("lua/TGNSCommon.lua")
 
 local notifiedPlayerIds = {}
 
-local originalNS2GRGetPlayerBannedFromCommand = TGNS.ReplaceClassMethod(DAK.config.loader.GamerulesClassName, "GetPlayerBannedFromCommand", function(self, playerId)
-	local player = DAK:GetPlayerMatching(playerId)
-	local numberOfNonRookiesOnTeam = #TGNS.GetMatchingClients(TGNS.GetPlayerList(), function(c,p) return TGNS.PlayersAreTeammates(player, p) and not TGNS.PlayerIsRookie(p) end)
-	local cancel = TGNS.PlayerIsRookie(player) and numberOfNonRookiesOnTeam >= 4
-	if cancel then
-		if not TGNS.Has(notifiedPlayerIds, playerId) then
-			TGNS.SendTeamChat(TGNS.GetPlayerTeamNumber(player), string.format("%s! Rookies will fight from the ground this game. Help them!", TGNS.GetPlayerTeamName(player)), "TGNS")
-			table.insert(notifiedPlayerIds, playerId)
-			TGNS.ScheduleAction(3, function() TGNS.RemoveAllMatching(notifiedPlayerIds, playerId) end)
+local originalGetIsPlayerValidForCommander
+originalGetIsPlayerValidForCommander = TGNS.ReplaceClassMethod("CommandStructure", "GetIsPlayerValidForCommander", function(self, player)
+	local result = originalGetIsPlayerValidForCommander(self, player)
+	if result then
+		local numberOfNonRookiesOnTeam = #TGNS.GetMatchingClients(TGNS.GetPlayerList(), function(c,p) return TGNS.PlayersAreTeammates(player, p) and not TGNS.PlayerIsRookie(p) end)
+		local numberOfAliveTeammates = #TGNS.Where(TGNS.GetPlayersOnSameTeam(player), TGNS.IsPlayerAlive)
+		local rookieShouldBePreventedFromCommanding = TGNS.PlayerIsRookie(player) and numberOfNonRookiesOnTeam >= 4 and numberOfAliveTeammates > 2
+		if rookieShouldBePreventedFromCommanding then
+			if not TGNS.Has(notifiedPlayerIds, playerId) then
+				TGNS.SendTeamChat(TGNS.GetPlayerTeamNumber(player), string.format("%s! Rookies will fight from the ground this game. Help them!", TGNS.GetPlayerTeamName(player)), "TGNS")
+				table.insert(notifiedPlayerIds, playerId)
+				TGNS.ScheduleAction(3, function() TGNS.RemoveAllMatching(notifiedPlayerIds, playerId) end)
+			end
 		end
+		result = not rookieShouldBePreventedFromCommanding
 	end
-	return cancel
+	return result
 end)
