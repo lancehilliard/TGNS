03dbd65bee571d0634cee16a362a617a5e532e3e
M. Lance Hilliard <lance@digitalcreations.cc>
2013-10-17 01:42:44 -0500

    voicecommreminder: command available to all

--- mods/tgns/output/lua/shine/extensions/voicecommreminder.lua
+++ mods/tgns/output/lua/shine/extensions/voicecommreminder.lua
@@ -5,18 +5,18 @@ local Plugin = {}
 function Plugin:CreateCommands()
 	local remindercommand = self:BindCommand( "sh_vr", "vr", function(client, playerPredicate)
 		local player = TGNS.GetPlayer(client)
-	if playerPredicate == nil or playerPredicate == "" then
-		md:ToPlayerNotifyError(player, "You must specify a player.")
-	else
-		local targetPlayer = TGNS.GetPlayerMatching(playerPredicate, nil)
-		if targetPlayer ~= nil then
-			md:ToTeamNotifyInfo(TGNS.GetPlayerTeamNumber(targetPlayer), string.format("%s: %s, make sure you can respond to voicecomm.", TGNS.GetClientName(client), TGNS.GetPlayerName(targetPlayer)))
-			md:ToTeamNotifyInfo(TGNS.GetPlayerTeamNumber(targetPlayer), "This server requires that from everyone. Press 'y' for team chat.")
+		if playerPredicate == nil or playerPredicate == "" then
+			md:ToPlayerNotifyError(player, "You must specify a player.")
 		else
-			md:ToPlayerNotifyError(player, string.format("'%s' does not uniquely match a player.", playerPredicate))
+			local targetPlayer = TGNS.GetPlayerMatching(playerPredicate, nil)
+			if targetPlayer ~= nil then
+				md:ToTeamNotifyInfo(TGNS.GetPlayerTeamNumber(targetPlayer), string.format("%s: %s, make sure you can respond to voicecomm.", TGNS.GetClientName(client), TGNS.GetPlayerName(targetPlayer)))
+				md:ToTeamNotifyInfo(TGNS.GetPlayerTeamNumber(targetPlayer), "This server requires that from everyone. Press 'y' for team chat.")
+			else
+				md:ToPlayerNotifyError(player, string.format("'%s' does not uniquely match a player.", playerPredicate))
+			end
 		end
-	end
-	end)
+	end, true)
 	remindercommand:AddParam{ Type = "string", TakeRestOfLine = true, Optional = true }
 	remindercommand:Help( "<player> Remind player of requirement to respond to voicecomm." )
 end
