7a53b6323c9019feba15d3eee74629d592cf8bd5
Timothy Moore <tjstretchalot@gmail.com>
2013-10-20 09:32:22 -0700

    Decrease time in an egg for aliens during bots

--- mods/tgns/output/lua/shine/extensions/bots.lua
+++ mods/tgns/output/lua/shine/extensions/bots.lua
@@ -4,6 +4,7 @@ local originalEndRoundOnTeamUnbalanceSetting = 0.4
 local originalForceEvenTeamsOnJoinSetting = true
 local originalAutoTeamBalanceSetting = { enabled_after_seconds = 10, enabled_on_unbalance_amount = 2 }
 local originalHatchCooldown = kHatchCooldown
+local originalAlienSpawnTime = kAlienSpawnTime
 local winOrLoseOccurredRecently
 local md
 local botAdvisory
@@ -35,6 +36,7 @@ local function setBotConfig()
 	Server.SetConfigSetting("auto_team_balance", {enabled_after_seconds=0, enabled_on_unbalance_amount=0})
 	
 	kHatchCooldown = 1
+	kAlienSpawnTime = 0.5
 end
 
 local function setOriginalConfig()
@@ -48,6 +50,7 @@ local function setOriginalConfig()
 		Server.SetConfigSetting("auto_team_balance", originalAutoTeamBalanceSetting)
 	end
 	kHatchCooldown = originalHatchCooldown
+	kAlienSpawnTime = originalAlienSpawnTime
 end
 
 local function removeBots(players, count)
