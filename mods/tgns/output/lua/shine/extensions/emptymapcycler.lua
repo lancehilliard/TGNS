211e16d5465048cbec97cf89d17418f67c459a72
M. Lance Hilliard <lance@digitalcreations.cc>
2013-11-04 00:30:30 -0600

    added: emptymapcycler

---
+++ mods/tgns/output/lua/shine/extensions/emptymapcycler.lua
@@ -0,0 +1,26 @@
+local mapMightBeBroken = true
+
+local Plugin = {}
+
+function Plugin:ClientConnect()
+	mapMightBeBroken = false
+end
+
+function Plugin:Initialise()
+    self.Enabled = true
+    TGNS.ScheduleAction(60, function()
+    	if mapMightBeBroken == true then
+            local nextMapName = TGNS.GetNextMapName()
+            Shared.Message(string.format("emptymapcycler cycling to %s...", nextMapName))
+	    	TGNS.SwitchToMap(nextMapName)
+    	end
+    end)
+    return true
+end
+
+function Plugin:Cleanup()
+    --Cleanup your extra stuff like timers, data etc.
+    self.BaseClass.Cleanup( self )
+end
+
+Shine:RegisterExtension("emptymapcycler", Plugin )
