aae7615c8abf8430cc19060fc2fe75cec4468cb7
M. Lance Hilliard <lance@digitalcreations.cc>
2013-11-26 00:23:17 -0600

    added: sh_showtimes

--- mods/tgns/output/lua/tgns/server/TGNSConnectedTimesTracker.lua
+++ mods/tgns/output/lua/tgns/server/TGNSConnectedTimesTracker.lua
@@ -32,7 +32,7 @@ function TGNSConnectedTimesTracker.GetClientConnectedTimeInSeconds(client)
 end
 
 local function GetTrackedClients()
-	local allConnectedClients = TGNS.GetClients(TGNS.GetPlayerList())
+	local allConnectedClients = TGNS.Where(TGNS.GetClients(TGNS.GetPlayerList()), function(c) return not TGNS.GetIsClientVirtual(c) end)
 	local result = TGNS.Where(allConnectedClients, function(c)
 		local steamId = TGNS.GetClientSteamId(c)
 		return connectedTimes[steamId] ~= nil
@@ -40,21 +40,21 @@ local function GetTrackedClients()
 	return result
 end
 
-//function TGNSConnectedTimesTracker.PrintConnectedDurations(client)
-//	local trackedClients = GetTrackedClients()
-//	TGNS.SortAscending(trackedClients, TGNSConnectedTimesTracker.GetClientConnectedTimeInSeconds)
-//	local trackedStrangers = TGNS.Where(trackedClients, TGNS.IsClientStranger)
-//	local trackedPrimerOnlys = TGNS.Where(trackedClients, TGNS.IsPrimerOnlyClient)
-//	local trackedSupportingMembers = TGNS.Where(trackedClients, TGNS.IsClientSM)
-//	local printConnectedTime = function(c)
-//		local connectedTimeInSeconds = TGNSConnectedTimesTracker.GetClientConnectedTimeInSeconds(c)
-//		TGNS.ConsolePrint(client, string.format("%s> %s: %s", TGNS.GetClientCommunityDesignationCharacter(c), TGNS.GetClientName(c), TGNS.SecondsToClock(Shared.GetSystemTime() - connectedTimeInSeconds)), "CONNECTEDTIMES")
-//	end
-//	TGNS.DoFor(trackedStrangers, printConnectedTime)
-//	TGNS.DoFor(trackedPrimerOnlys, printConnectedTime)
-//	TGNS.DoFor(trackedSupportingMembers, printConnectedTime)
-//end
-//TGNS.RegisterCommandHook("Console_sv_showtimes", TGNSConnectedTimesTracker.PrintConnectedDurations, "Print connected time of each client.")
+function TGNSConnectedTimesTracker.PrintConnectedDurations(client)
+	local trackedClients = GetTrackedClients()
+	TGNS.SortAscending(trackedClients, TGNSConnectedTimesTracker.GetClientConnectedTimeInSeconds)
+	local trackedStrangers = TGNS.Where(trackedClients, TGNS.IsClientStranger)
+	local trackedPrimerOnlys = TGNS.Where(trackedClients, TGNS.IsPrimerOnlyClient)
+	local trackedSupportingMembers = TGNS.Where(trackedClients, TGNS.IsClientSM)
+	local md = TGNSMessageDisplayer.Create("CONNECTEDTIMES")
+	local printConnectedTime = function(c)
+		local connectedTimeInSeconds = TGNSConnectedTimesTracker.GetClientConnectedTimeInSeconds(c)
+		md:ToClientConsole(client, string.format("%s> %s: %s", TGNS.GetClientCommunityDesignationCharacter(c), TGNS.GetClientName(c), TGNS.SecondsToClock(Shared.GetSystemTime() - connectedTimeInSeconds)))
+	end
+	TGNS.DoFor(trackedStrangers, printConnectedTime)
+	TGNS.DoFor(trackedPrimerOnlys, printConnectedTime)
+	TGNS.DoFor(trackedSupportingMembers, printConnectedTime)
+end
 
 local function SetClientLastSeenNow(client)
 	local steamId = TGNS.GetClientSteamId(client)
