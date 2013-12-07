a976ab584c0b3ad789c4a34f8ce0ee638e97a1be
M. Lance Hilliard <lance@digitalcreations.cc>
2013-12-07 15:59:08 -0600

    PlayerPreferredRepository: uses DataRepository

--- mods/tgns/output/lua/tgns/server/TGNSPlayerPreferredRepository.lua
+++ mods/tgns/output/lua/tgns/server/TGNSPlayerPreferredRepository.lua
@@ -1,10 +1,15 @@
-local DATA_FILENAME = "config://tgnsdata/PreferredPlayers.json"
 local isPreferredCache = {}
 
 TGNSPlayerPreferredRepository = {}
 
 function TGNSPlayerPreferredRepository.Create(preferredTypeName)
 	assert(preferredTypeName ~= nil and preferredTypeName ~= "")
+
+	local dr = TGNSDataRepository.Create("preferred", function(data)
+        data.preferreds = data.preferreds or {}
+        return data
+    end)
+
 	isPreferredCache[preferredTypeName] = {}
 	local result = {}
 	result.preferredTypeName = preferredTypeName
@@ -15,13 +20,10 @@ function TGNSPlayerPreferredRepository.Create(preferredTypeName)
 		if isPreferredCache[preferredTypeName][steamId] ~= nil then
 			isPreferred = isPreferredCache[preferredTypeName][steamId]
 		else
-			isPreferred = false
-			local dataFile = io.open(DATA_FILENAME, "r")
-			if dataFile then
-				local preferreds = json.decode(dataFile:read("*all")) or { }
-				dataFile:close()
-				isPreferred = TGNS.Any(preferreds, function(p) return p.plugin == self.preferredTypeName and p.id == steamId end)
-			end
+			local preferredData = dr.Load()
+			local preferreds = preferredData.preferreds
+			isPreferred = TGNS.Any(preferreds, function(p) return p.plugin == self.preferredTypeName and p.id == steamId end)
+			Shared.Message("IsClientPreferred: " .. steamId .. " for " .. self.preferredTypeName .. " = " .. tostring(isPreferred))
 			isPreferredCache[preferredTypeName][steamId] = isPreferred
 		end
 		return isPreferred
