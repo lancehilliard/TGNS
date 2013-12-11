bceb7e3195198496e148c6b68df1042426bb9c4e
M. Lance Hilliard <lance@digitalcreations.cc>
2013-12-10 23:34:24 -0600

    spawnselectionoverrides: build number config

--- mods/tgns/output/lua/shine/extensions/spawnselectionoverrides.lua
+++ mods/tgns/output/lua/shine/extensions/spawnselectionoverrides.lua
@@ -8,39 +8,45 @@ Plugin.ConfigName = "spawnselectionoverrides.json"
 
 function Plugin:Initialise()
     self.Enabled = true
-    md = TGNSMessageDisplayer.Create("SPAWNS")
+    md = TGNSMessageDisplayer.Create("CUSTOMSPAWNS")
 
 	originalResetGame = TGNS.ReplaceClassMethod("NS2Gamerules", "ResetGame", function(gamerules)
 		local success, result = xpcall(function()
-		    local spawnSelections = self.Config.spawnSelections[TGNS.GetCurrentMapName()]
-			if spawnSelections then
-				local spawnSelectionOverrides = {}
-				TGNS.DoFor(spawnSelections, function(s)
-					local techPointLocationNamesLower = TGNS.Select(TGNS.GetTechPointLocationNames(), function(n) return TGNS.ToLower(n) end)
-					if TGNS.Has(techPointLocationNamesLower, TGNS.ToLower(s[1])) and TGNS.Has(techPointLocationNamesLower, TGNS.ToLower(s[2])) and s[1] ~= s[2] then
-						local spawnSelectionOverride = {}
-						spawnSelectionOverride.marineSpawn = TGNS.ToLower(s[1])
-						spawnSelectionOverride.alienSpawn = TGNS.ToLower(s[2])
-						table.insert(spawnSelectionOverrides, spawnSelectionOverride)
+			if self.Config.enableForBuildNumber == Shared.GetBuildNumber() then
+			    local spawnSelections = self.Config.spawnSelections[TGNS.GetCurrentMapName()]
+				if spawnSelections then
+					local spawnSelectionOverrides = {}
+					TGNS.DoFor(spawnSelections, function(s)
+						local techPointLocationNamesLower = TGNS.Select(TGNS.GetTechPointLocationNames(), function(n) return TGNS.ToLower(n) end)
+						if TGNS.Has(techPointLocationNamesLower, TGNS.ToLower(s[1])) and TGNS.Has(techPointLocationNamesLower, TGNS.ToLower(s[2])) and s[1] ~= s[2] then
+							local spawnSelectionOverride = {}
+							spawnSelectionOverride.marineSpawn = TGNS.ToLower(s[1])
+							spawnSelectionOverride.alienSpawn = TGNS.ToLower(s[2])
+							table.insert(spawnSelectionOverrides, spawnSelectionOverride)
+						else
+							local message = string.format("%s spawnSelectionOverride %s/%s was discarded.", TGNS.GetCurrentMapName(), s[1], s[2])
+							TGNS.EnhancedLog("spawnselectionoverrides ERROR: " .. message)
+							md:ToAdminNotifyError(message)
+						end
+					end)
+					if originalSpawnSelectionOverrides == nil then
+						originalSpawnSelectionOverrides = Server.spawnSelectionOverrides
+					end
+					if #spawnSelectionOverrides >= 1 then
+						Server.spawnSelectionOverrides = spawnSelectionOverrides
+						if #spawnSelectionOverrides == 1 then
+							md:ToAdminNotifyInfo(string.format("NOTICE: %s only has 1 usable spawnSelectionOverride.", TGNS.GetCurrentMapName()))
+						end
 					else
-						TGNS.EnhancedLog(string.format("SPAWNS ERROR: %s spawnSelection %s/%s was not used.", TGNS.GetCurrentMapName(), s[1], s[2]))
+						md:ToAdminNotifyError(string.format("%s has spawnSelectionOverrides, but none is usable.", TGNS.GetCurrentMapName()))
 					end
-				end)
-				if originalSpawnSelectionOverrides == nil then
-					originalSpawnSelectionOverrides = Server.spawnSelectionOverrides
-				end
-				if #spawnSelectionOverrides >= 1 then
-					Server.spawnSelectionOverrides = spawnSelectionOverrides
-					if #spawnSelectionOverrides == 1 then
-						md:ToAdminNotifyInfo(string.format("SPAWNS NOTICE: %s only has 1 spawnSelectionOverride.", TGNS.GetCurrentMapName()))
-					end
-				else
-					md:ToAdminNotifyError(string.format("SPAWNS ERROR: %s has spawnSelectionOverrides, but none is usable.", TGNS.GetCurrentMapName()))
-				end
-		    end
+			    end
+			else
+				md:ToAdminNotifyError(string.format("Disabled! Configured for Build %s, but running Build %s.", self.Config.enableForBuildNumber, Shared.GetBuildNumber()))
+			end
 		end, debug.traceback)
 		if not success then
-			TGNS.EnhancedLog(string.format("spawnselectionoverrides Error (%s): %s", Shared.GetTime(), result))
+			TGNS.EnhancedLog(string.format("spawnselectionoverrides ERROR (%s): %s", Shared.GetTime(), result))
 		end
 		originalResetGame(gamerules)
 		-- TGNS.ScheduleAction(2, function()
