fbedf8cc5b5c85f0957694d6f421463e298b7205
M. Lance Hilliard <lance@digitalcreations.cc>
2014-02-15 23:55:18 -0600

    added forgotten files

---
+++ mods/tgns/output/lua/shine/extensions/disablestockmapvote.lua
@@ -0,0 +1,21 @@
+local md
+
+local Plugin = {}
+
+function Plugin:Initialise()
+    self.Enabled = true
+    md = TGNSMessageDisplayer.Create()
+	Server.HookNetworkMessage("VoteChangeMap", function(client)
+		local player = TGNS.GetPlayer(client)
+		md:ToPlayerNotifyError(player, "Built-in mapvoting is available when server mods fail to load.")
+		md:ToPlayerNotifyError(player, "Server mods loaded successfully, so vote starts are automated.")
+	end)
+    return true
+end
+
+function Plugin:Cleanup()
+    --Cleanup your extra stuff like timers, data etc.
+    self.BaseClass.Cleanup( self )
+end
+
+Shine:RegisterExtension("disablestockmapvote", Plugin )
---
+++ mods/tgns/output/lua/shine/extensions/friendlyfiretweaks.lua
@@ -0,0 +1,42 @@
+local md
+local clientFriendlyFireWarnings = {}
+
+local function TakeDamage( OldFunc, self, Damage, Attacker, Inflictor, Point, Direction, ArmourUsed, HealthUsed, DamageType, PreventAlert )
+	local victim = self
+	if Attacker and victim and Attacker:isa("Player") and victim ~= Attacker and Inflictor ~= nil and Inflictor:GetParent() == Attacker and victim:GetTeamNumber() == Attacker:GetTeamNumber() then
+		local attackerClient = TGNS.GetClient(Attacker)
+		if TGNS.IsClientStranger(attackerClient) then
+			clientFriendlyFireWarnings[attackerClient] = clientFriendlyFireWarnings[attackerClient] or 0
+			if clientFriendlyFireWarnings[attackerClient] < 2 then
+				clientFriendlyFireWarnings[attackerClient] = clientFriendlyFireWarnings[attackerClient] + 1
+				md:ToPlayerNotifyRed(Attacker, "Friendly fire ENABLED! Please check your fire.")
+			end
+		end
+		PreventAlert = true
+	end
+	return OldFunc( victim, Damage, Attacker, Inflictor, Point, Direction, ArmourUsed, HealthUsed, DamageType, PreventAlert )
+end
+
+local Plugin = {}
+
+function Plugin:PostJoinTeam(gamerules, player, oldTeamNumber, newTeamNumber, force, shineForce)
+    if TGNS.IsGameplayTeamNumber(newTeamNumber) and TGNS.IsClientStranger(TGNS.GetClient(client)) then
+    	TGNS.ScheduleAction(6, function()
+    		md:ToPlayerNotifyInfo(player, "Friendly Fire is ENABLED! Please be careful when attacking.")
+    	end)
+    end
+end
+
+function Plugin:Initialise()
+    self.Enabled = true
+	md = TGNSMessageDisplayer.Create("FF")
+	Shine.Hook.SetupClassHook( "LiveMixin", "TakeDamage", "TakeDamage", TakeDamage )
+    return true
+end
+
+function Plugin:Cleanup()
+    --Cleanup your extra stuff like timers, data etc.
+    self.BaseClass.Cleanup( self )
+end
+
+Shine:RegisterExtension("friendlyfiretweaks", Plugin )
