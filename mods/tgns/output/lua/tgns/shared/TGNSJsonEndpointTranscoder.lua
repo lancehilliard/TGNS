c934fcde7d10a49265ac87fb96e226cef34600e4
M. Lance Hilliard <lance@digitalcreations.cc>
2014-03-29 14:33:14 -0500

    JsonEndpointTranscoder: removed debug

--- mods/tgns/output/lua/tgns/shared/TGNSJsonEndpointTranscoder.lua
+++ mods/tgns/output/lua/tgns/shared/TGNSJsonEndpointTranscoder.lua
@@ -4,13 +4,8 @@ function TGNSJsonEndpointTranscoder.DecodeFromEndpoint(dataTypeName, recordId, c
 	callback = callback or function() end
 	local url = string.format("%s&d=%s&i=%s", TGNS.Config.DataEndpointBaseUrl, TGNS.UrlEncode(dataTypeName), TGNS.UrlEncode(recordId))
 	TGNS.GetHttpAsync(url, function(response)
-		-- Shared.Message("dataTypeName: " .. dataTypeName)
-		-- Shared.Message("recordId: " .. recordId)
-		-- Shared.Message("response: " .. response)
 		local result = json.decode(response) or {}
-		-- Shared.Message("success: " .. tostring(result.success))
 		if result.success then
-			-- Shared.Message("SUCCESS!")
 			result.value = result.value or {}
 		end
 		callback(result)
