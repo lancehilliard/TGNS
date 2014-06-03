9f09f5b38f1651a38c669c3d2c58948520f09ea2
M. Lance Hilliard <lance@digitalcreations.cc>
2014-06-03 02:02:08 -0500

    tgnsbadges: tweaked latest badge reminder timing

--- mods/tgns/output/lua/shine/extensions/tgnsbadges.lua
+++ mods/tgns/output/lua/shine/extensions/tgnsbadges.lua
@@ -66,7 +66,7 @@ end
 
 function Plugin:EndGame(gamerules, winningTeam)
 	if badgesModIsLoaded then
-		TGNS.ScheduleAction(TGNS.ENDGAME_TIME_TO_READYROOM + 51, function()
+		TGNS.ScheduleAction(TGNS.ENDGAME_TIME_TO_READYROOM + 53, function()
 			if Shine.Plugins.mapvote:VoteStarted() then
 				TGNS.DoFor(TGNS.GetClientList(), tellMostRecentBadge)
 			end
