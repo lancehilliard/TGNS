68ca1c8ab12e74c0067ce9258271f1d27a32758f
M. Lance Hilliard <lance@digitalcreations.cc>
2014-10-11 01:23:10 -0500

    speclisten: specmode option persistence

--- mods/tgns/output/lua/shine/extensions/speclisten.lua
+++ mods/tgns/output/lua/shine/extensions/speclisten.lua
@@ -8,6 +8,11 @@ local modeDescriptions = {["0"] = "Chat Advisory OFF; Voicecomm: All"
 	, ["4"] = "Chat Advisory OFF; Voicecomm: Spectators only"
 	, ["5"] = "Chat Advisory OFF; Voicecomm: None"}
 
+local pdr = TGNSPlayerDataRepository.Create("specmode", function(data)
+	data.specmode = data.specmode ~= nil and data.specmode or nil
+	return data
+end)
+
 local function listenerSpectatorShouldHearSpeaker(listenerPlayer, speakerPlayer)
 	local listenerClient = TGNS.GetClient(listenerPlayer)
 	local specmode = specmodes[listenerClient] or 0
@@ -24,6 +29,36 @@ end
 
 local Plugin = {}
 
+function Plugin:ClientConfirmConnect(client)
+	local steamId = TGNS.GetClientSteamId(client)
+	pdr:Load(steamId, function(loadResponse)
+		if Shine:IsValidClient(client) then
+			if loadResponse.success then
+				specmodes[client] = loadResponse.value.specmode
+			else
+				Shared.Message("specmode ERROR: Unable to access data.")
+			end
+		end
+	end)
+end
+
+local function showCurrentSpecMode(player, showIfNil)
+	local client = TGNS.GetClient(player)
+	local currentSpecMode = specmodes[client]
+	if showIfNil then
+		currentSpecMode = currentSpecMode or 0
+	end
+	if currentSpecMode then
+		md:ToPlayerNotifyInfo(player, string.format("Current sh_specmode: %s (%s)", currentSpecMode, modeDescriptions[tostring(currentSpecMode)]))
+	end
+end
+
+function Plugin:PostJoinTeam(gamerules, player, oldTeamNumber, newTeamNumber, force, shineForce)
+	if newTeamNumber == kSpectatorIndex then
+		showCurrentSpecMode(player)
+	end
+end
+
 function Plugin:CreateCommands()
     local modCommand = self:BindCommand( "sh_specmode", "specmode", function(client, modeCandidate)
     	local player = TGNS.GetPlayer(client)
@@ -32,9 +67,23 @@ function Plugin:CreateCommands()
     		showUsage(player)
     	else
     		specmodes[client] = mode
+    		local steamId = TGNS.GetClientSteamId(client)
+			pdr:Load(steamId, function(loadResponse)
+				if loadResponse.success then
+					local data = loadResponse.value
+					data.specmode = mode
+					pdr:Save(data, function(saveResponse)
+						if not saveResponse.success then
+							Shared.Message("specmode ERROR: Unable to save data.")
+						end
+					end)
+				else
+					taglineMd:ToPlayerNotifyError(TGNS.GetPlayer(client), "Unable to access tagline data.")
+					Shared.Message("taglines ERROR: Unable to access data.")
+				end
+			end)
     	end
-    	local currentSpecMode = tostring(specmodes[client] or 0)
-    	md:ToPlayerNotifyInfo(player, string.format("Current sh_specmode: %s (%s)", currentSpecMode, modeDescriptions[currentSpecMode]))
+    	showCurrentSpecMode(player, true)
     end, true)
     modCommand:AddParam{ Type = "string", TakeRestofLine = true, Optional = true }
     modCommand:Help( "Adjust what voicecomms you hear in Spectate." )
