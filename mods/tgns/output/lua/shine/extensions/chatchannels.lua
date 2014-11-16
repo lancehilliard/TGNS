e220a282382542a381836eb2b37b66311d3840ae
M. Lance Hilliard <lance@digitalcreations.cc>
2014-11-15 19:21:31 -0600

    chatchannels: fixed disconnected client defect

--- mods/tgns/output/lua/shine/extensions/chatchannels.lua
+++ mods/tgns/output/lua/shine/extensions/chatchannels.lua
@@ -59,9 +59,11 @@ Plugin.ConfigName = "chatchannels.json"
 function Plugin:ClientConfirmConnect(client)
 	local md = TGNSMessageDisplayer.Create("ADMINCHAT")
 	TGNS.ScheduleAction(7, function()
-		local totalGamesPlayed = Balance.GetTotalGamesPlayed(client)
-		if Shine:IsValidClient(client) and totalGamesPlayed > 0 and totalGamesPlayed < 50 then
-			md:ToPlayerNotifyInfo(TGNS.GetPlayer(client), "Any chat beginning with \"@\" shows only to admins. Try it!")
+		if Shine:IsValidClient(client) then
+			local totalGamesPlayed = Balance.GetTotalGamesPlayed(client)
+			if totalGamesPlayed > 0 and totalGamesPlayed < 50 then
+				md:ToPlayerNotifyInfo(TGNS.GetPlayer(client), "Any chat beginning with \"@\" shows only to admins. Try it!")
+			end
 		end
 	end)
 end
