2365cb6bfab32bf59ea7c8dbfbf79018c904b6ee
lancehilliard <lance@digitalcreations.cc>
2015-07-12 02:08:26 -0500

    CommonClient: help clients re-enable web windows

--- mods/tgns/output/lua/tgns/client/TGNSCommonClient.lua
+++ mods/tgns/output/lua/tgns/client/TGNSCommonClient.lua
@@ -6,5 +6,12 @@ function TGNS.SendNetworkMessage(messageName, variables)
 end
 
 function TGNS.ShowUrl(url, windowTitle)
-	Shine:OpenWebpage(url, windowTitle)
+	if Shine.Config.DisableWebWindows then
+		local prefix = "[TGNS]"
+		local message = string.format("Your web windows are disabled. To re-enable them, type 'sh_disableweb' in your console.")
+		Shine.AddChatText(240, 230, 130, prefix, 255, 255, 255, message)
+		Shared.Message(string.format("%s %s", prefix, message))
+	else
+		Shine:OpenWebpage(url, windowTitle)
+	end
 end
