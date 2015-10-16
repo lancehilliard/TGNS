25c86935943c5951aa98f5d8deddba664d13a613
lancehilliard <lance@digitalcreations.cc>
2015-10-15 22:56:30 -0500

    wraplength: TextWrap support

--- mods/tgns/output/lua/shine/extensions/wraplength/client.lua
+++ mods/tgns/output/lua/shine/extensions/wraplength/client.lua
@@ -2,29 +2,33 @@ local customLimit
 
 local Plugin = Plugin
 
+TGNS.HookNetworkMessage(Shine.Plugins.wraplength.WRAPLENGTH_DATA, function(message)
+    customLimit = message.l
+end)
+
 function Plugin:Initialise()
 	self.Enabled = true
-     --    local originalWrapText = WrapText
- --    WrapText = function( str, limit, indent, indent1 )
- --     if customLimit ~= nil and customLimit >= 40 then
- --         limit = customLimit
- --     end
- --     return originalWrapText( str, limit, indent, indent1 )
-    -- end
 
-    -- function WordWrap( Label, Text, XPos, MaxWidth )
-    local originalWordWrap = WordWrap
-    WordWrap = function( Label, Text, XPos, MaxWidth )
+    local getMaxWidth = function(MaxWidth)
         if customLimit then
             customLimit = Clamp( customLimit, self.MINIMUM_CHAT_WIDTH_PERCENTAGE, self.MAXIMUM_CHAT_WIDTH_PERCENTAGE )
             MaxWidth = Client.GetScreenWidth() * (customLimit/100)
         end
-        return originalWordWrap( Label, Text, XPos, MaxWidth )
+        return MaxWidth
     end
 
-    TGNS.HookNetworkMessage(Shine.Plugins.wraplength.WRAPLENGTH_DATA, function(message)
-        customLimit = message.l
-    end)
+    local originalWordWrapFunction = WordWrap
+    WordWrap = function( Label, Text, XPos, MaxWidth )
+        MaxWidth = getMaxWidth(MaxWidth)
+        return originalWordWrapFunction( Label, Text, XPos, MaxWidth )
+    end
+
+    local originalTextWrapFunction = TextWrap
+    TextWrap = function( Label, Text, XPos, MaxWidth )
+        MaxWidth = getMaxWidth(MaxWidth)
+        return originalTextWrapFunction( Label, Text, XPos, MaxWidth )
+    end
+
 	return true
 end
 
