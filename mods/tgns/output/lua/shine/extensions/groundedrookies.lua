579411afa8d51bbc28dbf703ee61627fdc78b71e
lancehilliard <lance@digitalcreations.cc>
2015-12-28 01:39:50 -0600

    groundedrookies: logic change

--- mods/tgns/output/lua/shine/extensions/groundedrookies.lua
+++ mods/tgns/output/lua/shine/extensions/groundedrookies.lua
@@ -1,23 +1,22 @@
-local notifiedPlayerIds = {}
-local originalGetIsPlayerValidForCommander
+--local notifiedPlayerIds = {}
 local md = TGNSMessageDisplayer.Create()
 
 local Plugin = {}
 
 function Plugin:Initialise()
     self.Enabled = true
+	local originalGetIsPlayerValidForCommander
 	originalGetIsPlayerValidForCommander = TGNS.ReplaceClassMethod("CommandStructure", "GetIsPlayerValidForCommander", function(self, player)
 		local result = originalGetIsPlayerValidForCommander(self, player)
 		if result then
-			local numberOfNonRookiesOnTeam = #TGNS.GetMatchingClients(TGNS.GetPlayerList(), function(c,p) return TGNS.PlayersAreTeammates(player, p) and not TGNS.PlayerIsRookie(p) end)
 			local numberOfAliveTeammates = #TGNS.Where(TGNS.GetPlayersOnSameTeam(player), TGNS.IsPlayerAlive)
-			local rookieShouldBePreventedFromCommanding = TGNS.PlayerIsRookie(player) and numberOfNonRookiesOnTeam >= 4 and numberOfAliveTeammates > 2
+			local rookieShouldBePreventedFromCommanding = TGNS.PlayerIsRookie(player) and Balance.GetTotalGamesPlayed(TGNS.GetClient(player)) < 20 and numberOfAliveTeammates > 2
 			if rookieShouldBePreventedFromCommanding then
-				if not TGNS.Has(notifiedPlayerIds, playerId) then
+				--if not TGNS.Has(notifiedPlayerIds, playerId) then
 					md:ToTeamNotifyError(TGNS.GetPlayerTeamNumber(player), string.format("%s! Rookies will fight from the ground this game. Help them!", TGNS.GetPlayerTeamName(player)))
-					table.insert(notifiedPlayerIds, playerId)
-					TGNS.ScheduleAction(3, function() TGNS.RemoveAllMatching(notifiedPlayerIds, playerId) end)
-				end
+					--table.insert(notifiedPlayerIds, playerId)
+					--TGNS.ScheduleAction(3, function() TGNS.RemoveAllMatching(notifiedPlayerIds, playerId) end)
+				--end
 			end
 			result = not rookieShouldBePreventedFromCommanding
 		end
