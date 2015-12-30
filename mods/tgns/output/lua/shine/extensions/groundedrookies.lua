ad7a02f302203bb8386f97e06e11412d684cd739
lancehilliard <lance@digitalcreations.cc>
2015-12-30 02:37:35 -0600

    groundedrookies: improved messaging

--- mods/tgns/output/lua/shine/extensions/groundedrookies.lua
+++ mods/tgns/output/lua/shine/extensions/groundedrookies.lua
@@ -13,7 +13,9 @@ function Plugin:Initialise()
 			local rookieShouldBePreventedFromCommanding = TGNS.PlayerIsRookie(player) and Balance.GetTotalGamesPlayed(TGNS.GetClient(player)) < 20 and numberOfAliveTeammates > 2
 			if rookieShouldBePreventedFromCommanding then
 				--if not TGNS.Has(notifiedPlayerIds, playerId) then
-					md:ToTeamNotifyError(TGNS.GetPlayerTeamNumber(player), string.format("%s: Rookies fight from the ground. Marines: help %s!", TGNS.GetPlayerName(player), TGNS.GetPlayerTeamName(player)))
+					local playerName = TGNS.GetPlayerName(player)
+					local teamName = TGNS.GetPlayerTeamName(player)
+					md:ToTeamNotifyError(TGNS.GetPlayerTeamNumber(player), string.format("%s: Rookies may not command. %s: help %s fight from the ground!", playerName, teamName, playerName))
 					--table.insert(notifiedPlayerIds, playerId)
 					--TGNS.ScheduleAction(3, function() TGNS.RemoveAllMatching(notifiedPlayerIds, playerId) end)
 				--end
