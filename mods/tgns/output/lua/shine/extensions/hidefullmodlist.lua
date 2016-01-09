5d4a3d68b66324ae1885cc405e68dabb0cd48fbd
lancehilliard <lance@digitalcreations.cc>
2016-01-09 01:13:09 -0600

    hidefullmodlist: http logging

--- mods/tgns/output/lua/shine/extensions/hidefullmodlist.lua
+++ mods/tgns/output/lua/shine/extensions/hidefullmodlist.lua
@@ -1,7 +1,18 @@
 local Plugin = {}
+local md = TGNSMessageDisplayer.Create()
+
+function Plugin:CreateCommands()
+	local sh_loghttp = self:BindCommand( "sh_loghttp", nil, function(client)
+		TGNS.LogHttp = not TGNS.LogHttp
+		md:ToClientConsole(client, string.format("TGNS.LogHttp: %s", TGNS.LogHttp == true))
+	end)
+	sh_loghttp:Help( "Log HTTP requests for map duration. Expensive." )
+end
+
 
 function Plugin:Initialise()
     self.Enabled = true
+    self:CreateCommands()
     TGNS.ScheduleAction(5, function()
     	if TGNS.GetSecondsSinceServerProcessStarted() < 30 then
     		TGNS.SwitchToMap(TGNS.GetCurrentMapName())
