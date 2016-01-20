029030fea9d8a275d3ae5e7d4fe16658045c26c7
lancehilliard <lance@digitalcreations.cc>
2016-01-20 01:32:52 -0600

    CommonServer: add SeparateThousands

--- mods/tgns/output/lua/tgns/shared/TGNSCommonShared.lua
+++ mods/tgns/output/lua/tgns/shared/TGNSCommonShared.lua
@@ -326,3 +326,13 @@ function TGNS.ToUpper(s)
 	return result
 end
 
+function TGNS.SeparateThousands(amount)
+  local formatted = amount
+  while true do  
+    formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+    if (k==0) then
+      break
+    end
+  end
+  return formatted
+end
