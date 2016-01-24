7631c29dc3964f38c95cb9e8aab94380de9f7849
lancehilliard <lance@digitalcreations.cc>
2016-01-24 00:32:00 -0600

    CommonServer: less http debug

--- mods/tgns/output/lua/tgns/server/TGNSCommonServer.lua
+++ mods/tgns/output/lua/tgns/server/TGNSCommonServer.lua
@@ -996,7 +996,7 @@ local function ProcessScheduledRequests() PROFILE("ProcessScheduledRequests")
 			r.sent = true
 			-- local requestStartTime = Shared.GetTime()
 			if TGNS.LogHttp or not TGNS.IsProduction() then
-				Shared.Message(string.format("TGNSCommonServer debug> http request #%s: %s (%s unsent)", numberOfHttpRequestsMade, r.url, #unsentRequests))
+				Shared.Message(string.format("TGNSCommonServer debug> http request #%s: %s", numberOfHttpRequestsMade, r.url))
 			end
 			numberOfHttpRequestsMade = numberOfHttpRequestsMade + 1
 			Shared.SendHTTPRequest(r.url, "GET", function(response)
