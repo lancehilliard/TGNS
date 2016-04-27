8413158408e614ee183a007509c5860ceb2baf93
lancehilliard <lance@digitalcreations.cc>
2016-04-27 14:51:48 -0500

    lookdown: disable

--- mods/tgns/output/lua/shine/extensions/lookdown.lua
+++ mods/tgns/output/lua/shine/extensions/lookdown.lua
@@ -7,23 +7,23 @@ function Plugin:Initialise()
     return true
 end
 
-function Plugin:OnProcessMove(player, input)
-	if player then
-		local playerIsLookingDown = not TGNS.IsPlayerSpectator(player) and (input.pitch < 1.6 and input.pitch > 1.2)
-		local clientIndex = player:GetClientIndex()
-		if clientIndex then
-			if lastKnownStatuses[clientIndex] ~= playerIsLookingDown then
-				TGNS.ExecuteEventHooks("LookDownChanged", player, playerIsLookingDown)
-				lastKnownStatuses[clientIndex] = playerIsLookingDown
-			end
-		end
-	end
-end
+-- function Plugin:OnProcessMove(player, input)
+-- 	if player then
+-- 		local playerIsLookingDown = not TGNS.IsPlayerSpectator(player) and (input.pitch < 1.6 and input.pitch > 1.2)
+-- 		local clientIndex = player:GetClientIndex()
+-- 		if clientIndex then
+-- 			if lastKnownStatuses[clientIndex] ~= playerIsLookingDown then
+-- 				TGNS.ExecuteEventHooks("LookDownChanged", player, playerIsLookingDown)
+-- 				lastKnownStatuses[clientIndex] = playerIsLookingDown
+-- 			end
+-- 		end
+-- 	end
+-- end
 
-function Plugin:IsPlayerLookingDown(player)
-	local result = player and lastKnownStatuses[player:GetClientIndex()] == true
-	return result
-end
+-- function Plugin:IsPlayerLookingDown(player)
+-- 	local result = player and lastKnownStatuses[player:GetClientIndex()] == true
+-- 	return result
+-- end
 
 function Plugin:Cleanup()
     --Cleanup your extra stuff like timers, data etc.
