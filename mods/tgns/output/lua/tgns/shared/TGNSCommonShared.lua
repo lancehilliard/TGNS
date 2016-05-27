1fddcbea836fc22288943423d502c156369494bc
lancehilliard <lance@digitalcreations.cc>
2016-05-27 16:23:43 -0500

    CommonShared: add UrlEncode

--- mods/tgns/output/lua/tgns/shared/TGNSCommonShared.lua
+++ mods/tgns/output/lua/tgns/shared/TGNSCommonShared.lua
@@ -338,3 +338,13 @@ function TGNS.SeparateThousands(amount)
   end
   return formatted
 end
+
+function TGNS.UrlEncode(str) PROFILE("TGNS.UrlEncode")
+  if (str) then
+    str = string.gsub (str, "\n", "\r\n")
+    str = string.gsub (str, "([^%w %-%_%.%~])",
+        function (c) return string.format ("%%%02X", string.byte(c)) end)
+    str = string.gsub (str, " ", "+")
+  end
+  return str
+end
