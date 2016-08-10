66578a33db542eaa1a7b36a2d1837b58fe7923fa
lancehilliard <lance@digitalcreations.cc>
2016-08-10 00:27:47 -0500

    hidefullmodlist: better restart handling

--- mods/tgns/output/lua/shine/extensions/hidefullmodlist.lua
+++ mods/tgns/output/lua/shine/extensions/hidefullmodlist.lua
@@ -1,5 +1,6 @@
 local Plugin = {}
 local md = TGNSMessageDisplayer.Create()
+local NUMBER_OF_ACTIVE_MODS_INDICATIVE_OF_A_SERVER_RESTART = 15
 
 function Plugin:CreateCommands()
 	local sh_loghttp = self:BindCommand( "sh_loghttp", nil, function(client)
@@ -13,10 +14,24 @@ end
 function Plugin:Initialise()
     self.Enabled = true
     self:CreateCommands()
-    TGNS.ScheduleAction(5, function()
-    	if TGNS.GetSecondsSinceServerProcessStarted() < 15 then
-    		TGNS.SwitchToMap(TGNS.GetCurrentMapName())
-    	end
+    TGNS.ScheduleAction(0, function()
+        if TGNS.GetSecondsSinceServerProcessStarted() < 60 then
+            Shared.Message("Server.GetNumActiveMods(): " .. tostring(Server.GetNumActiveMods()))
+            Shared.Message("TGNS.GetSecondsSinceServerProcessStarted(): " .. tostring(TGNS.GetSecondsSinceServerProcessStarted()))
+            local serverHasJustStartedAndHasMadeAllModsActiveOnFirstMap = Server.GetNumActiveMods() >= NUMBER_OF_ACTIVE_MODS_INDICATIVE_OF_A_SERVER_RESTART
+            if serverHasJustStartedAndHasMadeAllModsActiveOnFirstMap then
+                Shared.Message("Switching first map to prevent all clients from having to download all mods...")
+                TGNS.SwitchToMap(TGNS.GetCurrentMapName())
+            end
+            TGNS.RegisterEventHook("ClientConfirmConnect", function(client)
+                if TGNS.GetSecondsSinceServerProcessStarted() < 300 then
+                    Shine.ScreenText.Add(71, {X = 0.3, Y = 0.3, Text = "This server recently crashed/started.", Duration = 10, R = 0, G = 255, B = 0, Alignment = TGNS.ShineTextAlignmentMin, Size = 3, FadeIn = 0, IgnoreFormat = true}, client)
+                end
+            end)
+            TGNS.ScheduleAction(10, function()
+                Shine.Plugins.push:Push("tgns-admin", "Server started.", string.format("%s on %s. Server Info: http://rr.tacticalgamer.com/ServerInfo", TGNS.GetCurrentMapName(), TGNS.GetSimpleServerName()))
+            end)
+        end
     end)
     return true
 end
