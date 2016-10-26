36dddca1535f45ff1f59274a2d70910f94bac7b2
lancehilliard <lance@digitalcreations.cc>
2016-10-26 02:33:28 -0500

    scoreboard: add My Tagline to scoreboard menu

--- mods/tgns/output/lua/shine/extensions/scoreboard/client.lua
+++ mods/tgns/output/lua/shine/extensions/scoreboard/client.lua
@@ -1147,6 +1147,7 @@ function Plugin:Initialise()
 				  , {text="  Portal: My Badges", callback=function(data) Client.ShowWebpage("http://rr.tacticalgamer.com/Badges/Manage") end, condition=targetIsSelf}
 				  , {text="  Portal: My Karma", callback=function(data) Client.ShowWebpage("http://rr.tacticalgamer.com/Karma") end, condition=targetIsSelf}
 				  , {text="  Portal: My Laps", callback=function(data) Client.ShowWebpage("http://rr.tacticalgamer.com/Laps/Manage") end, condition=targetIsSelf}
+				  , {text="  Portal: My Tagline", callback=function(data) Client.ShowWebpage("http://rr.tacticalgamer.com/Tagline/Manage") end, condition=targetIsSelf}
 				  , {text="  Portal: My Settings", callback=function(data) Client.ShowWebpage("http://rr.tacticalgamer.com/My/Settings") end, condition=targetIsSelf}
 				  , {text="  Send to RR (pre/early-game AFK)", callback=function(data) TGNS.SendNetworkMessage(Plugin.REQUEST_AFKRR, {c=self.hoverPlayerClientIndex}) end, condition=targetIsEligibleForAfkRr}
 				  , {text="  Admin Feedback", callback=function(data) Client.ShowWebpage(string.format("http://rr.tacticalgamer.com/Feedback/Index?i=%s&n=%s&s=%s", data.ns2id, url_encode(data.playerName), url_encode(data.serverSimpleName))) end, condition=not targetIsSelf}
