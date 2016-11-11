1b7318a212e486e23a09445d1228a20af83fe846
lancehilliard <lance@digitalcreations.cc>
2016-11-11 16:35:06 -0600

    restartwhenempty: use TGNS.RestartServerProcess()

--- mods/tgns/output/lua/shine/extensions/restartwhenempty/server.lua
+++ mods/tgns/output/lua/shine/extensions/restartwhenempty/server.lua
@@ -13,15 +13,7 @@ function Plugin:Initialise()
     TGNS.ScheduleAction(5, function()
 	    TGNS.ScheduleAction(TGNS.ConvertMinutesToSeconds(Shine.Plugins.mapvote.MapCycle.time - 1), function()
 			if not clientConnected and TGNS.ConvertSecondsToHours(TGNS.GetSecondsSinceServerProcessStarted()) > 2 then
-				local serverName = TGNS.Config.ServerSimpleName
-				local url = string.format("%s&s=%s&c=%s&t=1&a=false", TGNS.Config.ServerCommandEndpointBaseUrl, TGNS.UrlEncode(serverName), TGNS.UrlEncode("sh_warnrestart"))
-				TGNS.DebugPrint("restart url: " .. tostring(url))
-				TGNS.GetHttpAsync(url, function(responseJson)
-					local response = json.decode(responseJson) or {}
-					if not response.success then
-						TGNS.DebugPrint(string.format("restartwhenempty ERROR: Unable to request restart. msg: %s | response: %s | stacktrace: %s | url: %s", response.msg, responseJson, response.stacktrace, url))
-					end
-				end)
+				TGNS.RestartServerProcess()
 			end
 	    end)
     end)
