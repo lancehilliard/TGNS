67aadb1540792b898e144265983f57b6421e4caa
lancehilliard <lance@digitalcreations.cc>
2017-01-18 11:07:57 -0600

    serverstart: more scheduled restarts :(

--- mods/tgns/output/lua/shine/extensions/serverstart/server.lua
+++ mods/tgns/output/lua/shine/extensions/serverstart/server.lua
@@ -4,7 +4,7 @@ Plugin.HasConfig = false
 local md = TGNSMessageDisplayer.Create()
 local serverStartTempfilePath = "config://tgns/temp/serverStart.json"
 local NUMBER_OF_ACTIVE_MODS_INDICATIVE_OF_A_SERVER_RESTART = 15
-local NUMBER_OF_HOURS_SERVER_SEEMS_TO_SURVIVE_WITHOUT_CRASHING = 3
+local NUMBER_OF_HOURS_SERVER_SEEMS_TO_SURVIVE_WITHOUT_CRASHING = 2.5
 local SERVER_COMMANDLINE_START_MAP_NAME = "ns2_tram"
 
 function Plugin:ClientConfirmConnect(client)
